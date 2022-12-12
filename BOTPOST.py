e4f3199617fce72ee35742c0999ac18038e79c6a
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2022-12-12 15:29:35 -0400

    Update BOTPOST.py

--- BOTPOST.py
+++ BOTPOST.py
@@ -82,7 +82,7 @@ if __name__ == '__main__':
   for symbol in symbols:
         indicator(symbol)
       
- schedule.every(10).seconds.do(indicator)
+ schedule.every(10).seconds.do()
 
 while True:
     schedule.run_pending()

baa8459c4440390a101df1abffa2a36188c8d073
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2022-12-12 15:57:56 -0400

    Update BOTPOST.py

--- BOTPOST.py
+++ BOTPOST.py
@@ -78,16 +78,9 @@ if __name__ == '__main__':
   ]
 #symbols = ["BTCUSDT", "TRXUSDT", "BNBUSDT", "ETHUSDT", "ETCUSDT", "MATICUSDT", "XRPUSDT", "AVAXUSDT", "DOGEUSDT", "ADAUSDT"]
 
-def server_time():
-  time_server = client.get_server_time()
-  time = pd.to_datetime(time_server["serverTime"], unit="ms")
-  minute = int(time.strftime("%M"))
- 
+
   for i in intervals:
-    if minute == i and second == 3:
+  
       for symbol in symbols:
         indicator(symbol)
-
-while (True):
-  server_time()
-  
+        time.sleep(3)

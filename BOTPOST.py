112e5738bb9bedf860cbd7cca07fe1ade9a9e9e2
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2022-12-12 15:03:33 -0400

    Update BOTPOST.py

--- BOTPOST.py
+++ BOTPOST.py
@@ -94,7 +94,7 @@ def server_time():
   time_ = time.strftime("%H:%M:%S")
 
   for i in intervals:
-    if minute == i:
+   if minute == i and second == 3:
       for symbol in symbols:
         indicator(symbol)
 keep_alive()

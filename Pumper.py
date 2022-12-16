a50698e357d8f208696dc51e2783c501df74f8ec
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2022-12-16 18:01:20 -0400

    Update Pumper.py

--- Pumper.py
+++ Pumper.py
@@ -68,10 +68,10 @@ def indicator(symbol):
    
   #print(df_new['Volume'][-1])
   
-  if (middleband[-2:].values[0] >= ema[-1:].values[0]) and (ema[-1:].values[0] > middleband[-1:].values[0]) and (rsi[-2:].values[0] >= 20 > rsi[-1:].values[0]) and (mfi[-2:].values[0] >= 20 > mfi[-1:].values[0]):
+  if (middleband[-2:].values[0] >= ema[-1:].values[0]) and (ema[-1:].values[0] > middleband[-1:].values[0]) and (rsi[-2:].values[0] >= 20 > rsi[-1:].values[0]):
     requests.post('https://hook.finandy.com/yOiR__CztpkFLaRKqFUK', json=CORTO)
     Tb.telegram_send_message(" 🔴 SHORT  " + symbol + "\n 💵 Precio: " + df['Close'][-1])
-  elif (middleband[-2:].values[0] <= ema[-1:].values[0]) and ( ema[-1:].values[0] < middleband[-1:].values[0]) and (rsi[-2:].values[0] <= 80 < rsi[-1:].values[0]) and (mfi[-2:].values[0] <= 80 < mfi[-1:].values[0]): 
+  elif (middleband[-2:].values[0] <= ema[-1:].values[0]) and ( ema[-1:].values[0] < middleband[-1:].values[0]) and (rsi[-2:].values[0] <= 80 < rsi[-1:].values[0]): 
     requests.post('https://hook.finandy.com/A81ci3iO7HXP9btKqFUK', json=LARGO)
     Tb.telegram_send_message(" 🟢 LONG  " + symbol + "\n 💵 Precio: " + df['Close'][-1])
     

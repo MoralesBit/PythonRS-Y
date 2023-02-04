0b98b7cc0d0048360539adcf66c3356b1127c50d
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-02-04 13:18:05 -0400

    Update BOTPOST.py

--- BOTPOST.py
+++ BOTPOST.py
@@ -7,7 +7,6 @@ import  schedule as schedule
 import time as ti
 import requests
 
-
 Pkey = ''
 Skey = ''
 
@@ -20,7 +19,7 @@ connection = ""
 period = 14
 def indicator(symbol):
   rsi_stat = ""
-  kline = client.futures_historical_klines(symbol, "3m", "2 hours ago UTC+1",limit=100)
+  kline = client.futures_historical_klines(symbol, "5m", "2 hours ago UTC+1",limit=100)
   df = pd.DataFrame(kline)
   if not df.empty:
     df.columns = [
@@ -34,40 +33,53 @@ def indicator(symbol):
     
   upperband, middleband, lowerband = ta.BBANDS(df['Close'],
                                                timeperiod=20,
-                                               nbdevup=3,
-                                               nbdevdn=3,
+                                               nbdevup=2,
+                                               nbdevdn=2,
                                                matype=0)
   roc = ta.ROC(df['Close'], timeperiod=10)
-  Open = float(df['Open'][-1])
-  Close = float(df['Close'][-2])
+  adx = ta.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
+  mfi = ta.MFI(df['High'], df['Low'], df['Close'], df['Volume'], timeperiod=14)
+  df['EMA'] = ta.EMA(df['Close'], timeperiod = 30)
+  #bars = client.futures_ticker()
+  #df_new = pd.DataFrame(bars, columns=['Open', 'High', 'Low', 'Close', 'Volume'])
+  Close = float(df['Close'][-1])
   High = float(df['High'][-1])
   Low = float(df['Low'][-1])
   diff = abs((High / Low -1) * 100)
   slowk, slowd = ta.STOCH(df['High'], df['Low'], df['Close'], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+  Open = float(df['Open'][-1])
+  df['MA100'] = df['Close'].ewm(span=100).mean()
+  df['MA13'] = df['Close'].ewm(span=13).mean()
+  macd, signal, hist = ta.MACD(df['Close'], 
+                                    fastperiod=12, 
+                                    slowperiod=26, 
+                                    signalperiod=9)
   
-    
-  CORTO = {
-  "name": "USUARIO 002 - CORTO",
+  BB = ((Close - lowerband[-1])/(upperband[-1] - lowerband[-1])) 
+  
+  print(symbol)
+ 
+  
+  SHORT = {
+  "name": "EMA 13-100 SHORT",
   "secret": "hgw3399vhh",
   "side": "sell",
-  "symbol": symbol,
-  "open": { "price": "" }
-  }
-  LARGO = {
-  "name": "USUARIO 002 - LARGO",
+  "symbol": symbol
+}
+
+  LONG = {
+  "name": "EMA 13-100 LONG",
   "secret": "xjth0i3qgb",
   "side": "buy",
   "symbol": symbol
-  }
- 
-  print(symbol)
-   
-    if (diff > 3) and (rsi[-2] < 20) and (slowk[-2] < 20) and (lowerband[-2] >= Close):
-      requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=LARGO)
-      Tb.telegram_send_message(" ⚡️ " + symbol + "\n 🌵 LONG \n 💵 Precio: " + df['Close'][-1] + "\n 🔃 % ROC: " + str(round(roc[-1],2)) + "\n 📉 RSI : " + str(round(rsi[-1],2)))
-    elif (diff > 3) and (rsi[-2] > 80) and (slowk[-2] < 80) and (upperband[-2] < Close):
-      requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=CORTO)
-      Tb.telegram_send_message(" ⚡️ " + symbol + "\n 🩸 SHORT \n 💵 Precio: " + df['Close'][-1] + "\n 🔃 % ROC: " + str(round(roc[-1],2)) + "\n 📈 RSI : " + str(round(rsi[-1],2)))
+}
+    
+  if (df['MA13'][-1] < df['MA100'][-1]) and (df['MA100'][0] < df['MA13'][0]) and (macd[-1] < macd[0]):
+      requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=LONG)
+      Tb.telegram_send_message(" Cruce EMA 13-100 " + symbol + "\n 🟢 LONG \n 💵 Precio: " + df['Close'][-1])
+  elif (df['MA13'][-1] > df['MA100'][-1]) and (df['MA100'][0] > df['MA13'][0]) and (macd[-1] > macd[0]):
+      requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=SHORT)  
+      Tb.telegram_send_message(" Cruce EMA 13-100 " + symbol + "\n 🔴 SHORT \n 💵 Precio: " + df['Close'][-1])
 
   return round(last_rsi, 1), rsi_stat
 
@@ -84,11 +96,12 @@ def server_time():
   
   for symbol in symbols:
     indicator(symbol)
-    ti.sleep(0.01)
+    ti.sleep(0.1)
         
-   
+        
 while (True):
   
   server_time()
   ti.sleep(60)
+
   

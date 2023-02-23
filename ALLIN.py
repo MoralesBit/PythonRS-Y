e0a64ac627278c1861471b8ebd01a7a8884de4f3
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-02-23 00:59:47 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -88,10 +88,10 @@ def indicator(symbol):
   "symbol": symbol
 }
   
-  if (cci[-2] < 0) and (cci[-1] > 0) and (Close > (df['MA50'][-1])) and ((df['MA50'][-2]) < (df['MA50'][-1])) and ( macd[-1] > signal[-1]) and (hist[-2] < hist[-1]):
+  if (macd[-2] < signal[-2]) and (macd[-1] > signal[-1]) and (hist[-2] < 0) and (hist[-1] > 0) and (Close > df['MA50'][-1]):
       requests.post('https://hook.finandy.com/VMfD-y_3G5EgI5DUqFUK', json=CCILONG)
       Tb.telegram_send_message( "🎱 " + symbol + "\n🟢 ALCISTA \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n⚠️ No Operar")
-  elif (cci[-2] > 0) and (cci[-1] < 0) and (Close < (df['MA50'][-1])) and ((df['MA50'][-2]) > (df['MA50'][-1])) and ( macd[-1] < signal[-1]) and (hist[-2] > hist[-1]):
+  elif (macd[-2] > signal[-2]) and (macd[-1] < signal[-1]) and (hist[-2] > 0) and (hist[-1] < 0) and (Close < df['MA50'][-1]):
       requests.post('https://hook.finandy.com/gZZtqWYCtUdF0WwyqFUK', json=CCISHORT)  
       Tb.telegram_send_message( "🎱 " + symbol + "\n🔴 BAJISTA \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n⚠️ No Operar")
     

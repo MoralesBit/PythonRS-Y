0a0a7a54bf5a02678b5cd351daa4a93f36fceba5
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-02-24 16:03:19 -0400

    Update Telegram_bot.py

--- Telegram_bot.py
+++ Telegram_bot.py
@@ -6,3 +6,10 @@ def telegram_send_message(message):
     send_text = "https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+bot_chatID+"&parse_mode=Markdown&text="+message
     response = requests.get(send_text)
     return response.json()
+
+def telegram_canal_prueba(message):
+    bot_token = "5028180661:AAHM9j4jxsU_bCLpxbNjOtP5DIAT_UgUs-s"
+    bot_chatID = "-1001721640586"
+    send_text = "https://api.telegram.org/bot"+bot_token+"/sendMessage?chat_id="+bot_chatID+"&parse_mode=Markdown&text="+message
+    response = requests.get(send_text)
+    return response.json()

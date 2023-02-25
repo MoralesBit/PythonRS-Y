34ff5998a9e87f5658adcf5c7bdd40d2997ac3fe
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-02-25 13:27:45 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -101,11 +101,11 @@ def indicator(symbol):
   
   #EMA 13  
   if (df['Positions'][-1] == 1.0):
-      requests.post('https://hook.finandy.com/VMfD-y_3G5EgI5DUqFUK', json=CCILONG)
+      #requests.post('https://hook.finandy.com/VMfD-y_3G5EgI5DUqFUK', json=CCILONG)
       Tb.telegram_canal_prueba( "EMA 13-100: \n" + symbol + "\n🟢 LONG \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n EMA 13 " + str(round((df['EMA13'][-1]),3)) + "\n EMA 100: " + str(round((df['EMA100'][-1]),3)))
        
   if (df['Positions'][-1] == -1.0):
-      requests.post('https://hook.finandy.com/gZZtqWYCtUdF0WwyqFUK', json=CCISHORT)  
+      #requests.post('https://hook.finandy.com/gZZtqWYCtUdF0WwyqFUK', json=CCISHORT)  
       Tb.telegram_canal_prueba( "EMA 13-100: \n" + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n EMA 13 " + str(round((df['EMA13'][-1]),3)) + "\n EMA 100: " + str(round((df['EMA100'][-1]),3)))
        
   #Tendencia ORIGINAL    

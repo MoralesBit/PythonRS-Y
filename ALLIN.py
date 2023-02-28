966445d406768954a21d7f3a621994617a2f219d
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-02-28 11:35:26 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -28,32 +28,7 @@ def indicator(symbol):
       'Quote_Volume', 'Trades_Count', 'BUY_VOL', 'BUY_VOL_VAL', 'x']
     df['Date'] = pd.to_datetime(df['Date'], unit='ms')
     df = df.set_index('Date')
-  
-  info = client.futures_historical_klines("BTCUSDT", "15m", "24 hours ago UTC+1",limit=1000) 
-  df_new = pd.DataFrame(info)
-       
-  if not df_new.empty:
-        df_new.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close' 'IGNORE',
-      'Quote_Volume', 'Trades_Count', 'BUY_VOL', 'BUY_VOL_VAL', 'x']
-  df_new['Date'] = pd.to_datetime(df_new['Date'], unit='ms')
-  df_new = df_new.set_index('Date')
-  macdB, signalB, histB = ta.MACD(df_new['Close'], 
-                                    fastperiod=12, 
-                                    slowperiod=26, 
-                                    signalperiod=9)   
     
-  rsi = ta.RSI(df["Close"], timeperiod=period)
- 
-  last_rsi = rsi   
-  upperband, middleband, lowerband = ta.BBANDS(df['Close'],
-                                               timeperiod=20,
-                                               nbdevup=2,
-                                               nbdevdn=2,
-                                               matype=0)
- 
-  #roc = ta.ROC(df['Close'], timeperiod=10)
-  #adx = ta.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
-  #mfi = ta.MFI(df['High'], df['Low'], df['Close'], df['Volume'], timeperiod=14)
   df['EMA13'] = df['Close'].ewm(13).mean()
   df['EMA50'] = df['Close'].ewm(50).mean()
   df['EMA100'] = df['Close'].ewm(100).mean()
@@ -63,31 +38,51 @@ def indicator(symbol):
     
   df['signal'] = np.where(df['EMA13'] > df['EMA100'], 1,0)
   
-  df['Positions'] = df['signal'].diff()
+  df['Positions'] = df['signal'].diff()  
   
-  
-   
-  #Close = float(df['Close'][-1])
-  #High = float(df['High'][-1])
-  #Low = float(df['Low'][-1])
-  #Open = float(df['Open'][-1])
-  #diff = abs((High / Low -1) * 100)
-  
-  #slowk, slowd = ta.STOCH(df['High'], df['Low'], df['Close'], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
-  
-
+  upperband, middleband, lowerband = ta.BBANDS(df['Close'],
+                                               timeperiod=20,
+                                               nbdevup=2,
+                                               nbdevdn=2,
+                                               matype=0)
   macd, signal, hist = ta.MACD(df['Close'], 
                                     fastperiod=12, 
                                     slowperiod=26, 
                                     signalperiod=9)
    
   cci = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=14)
+  
+  rsi = ta.RSI(df["Close"], timeperiod=period)
+ 
+  last_rsi = rsi 
+  
+  info = client.futures_historical_klines("BTCUSDT", "15m", "24 hours ago UTC+1",limit=1000) 
+  df_new = pd.DataFrame(info)
+       
+  if not df_new.empty:
+        df_new.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close' 'IGNORE',
+      'Quote_Volume', 'Trades_Count', 'BUY_VOL', 'BUY_VOL_VAL', 'x']
+  df_new['Date'] = pd.to_datetime(df_new['Date'], unit='ms')
+  df_new = df_new.set_index('Date')
   cciB = ta.CCI(df_new['High'], df_new['Low'], df_new['Close'], timeperiod=14)
+  macdB, signalB, histB = ta.MACD(df_new['Close'], 
+                                    fastperiod=12, 
+                                    slowperiod=26, 
+                                    signalperiod=9)   
+  #roc = ta.ROC(df['Close'], timeperiod=10)
+  #adx = ta.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
+  #mfi = ta.MFI(df['High'], df['Low'], df['Close'], df['Volume'], timeperiod=14)
+  #Close = float(df['Close'][-1])
+  #High = float(df['High'][-1])
+  #Low = float(df['Low'][-1])
+  #Open = float(df['Open'][-1])
+  #diff = abs((High / Low -1) * 100)
+  #slowk, slowd = ta.STOCH(df['High'], df['Low'], df['Close'], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
   #atr = ta.ATR(df['High'], df['Low'], df['Close'], timeperiod=14)
   #tra = ta.TRANGE(df['High'], df['Low'], df['Close'])
   
   print(symbol)
-  print(df['Positions'][-2])
+  print(rsi[-1])
        
   CCISHORT = {
   "name": "CCI SHORT",

d03c0c30627405dfc9549d1f860d253763fe2499
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-02-28 21:45:32 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -109,12 +109,12 @@ def indicator(symbol):
        
   #Tendencia ORIGINAL    
   if (cciB[-2] < 0) and (cciB[-1] > 0) and (histB[-1] > 0.2):
-    if (cci[-1] > 70):
+    if (cci[-1] > 60):
       requests.post('https://hook.finandy.com/VMfD-y_3G5EgI5DUqFUK', json=CCILONG)
       Tb.telegram_send_message( "🎱 " + symbol + "\n🟢 LONG \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n⚠️ No Operar \nBOT TENDENCIA")
        
   if  (cciB[-2] > 0) and (cciB[-1] < 0) and (histB[-1] < -0.2): 
-    if (cci[-1] < -70):
+    if (cci[-1] < -60):
       requests.post('https://hook.finandy.com/gZZtqWYCtUdF0WwyqFUK', json=CCISHORT)  
       Tb.telegram_send_message( "🎱 " + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n⚠️ No Operar \nBOT TENDENCIA")
     

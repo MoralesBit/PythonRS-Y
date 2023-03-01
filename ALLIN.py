2f7eb832ea9efbf5542dc8a8c3e701045323133b
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-02-28 21:42:37 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -108,12 +108,12 @@ def indicator(symbol):
       Tb.telegram_canal_prueba( "EMA 13-100: \n" + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n EMA 13 " + str(round((df['EMA13'][-1]),3)) + "\n EMA 100: " + str(round((df['EMA100'][-1]),3)))
        
   #Tendencia ORIGINAL    
-  if (cciB[-2] < 0) and (cciB[-1] > 0) and (histB[-1] > 0):
+  if (cciB[-2] < 0) and (cciB[-1] > 0) and (histB[-1] > 0.2):
     if (cci[-1] > 70):
       requests.post('https://hook.finandy.com/VMfD-y_3G5EgI5DUqFUK', json=CCILONG)
       Tb.telegram_send_message( "🎱 " + symbol + "\n🟢 LONG \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n⚠️ No Operar \nBOT TENDENCIA")
        
-  if  (cciB[-2] > 0) and (cciB[-1] < 0) and (histB[-1] < 0): 
+  if  (cciB[-2] > 0) and (cciB[-1] < 0) and (histB[-1] < -0.2): 
     if (cci[-1] < -70):
       requests.post('https://hook.finandy.com/gZZtqWYCtUdF0WwyqFUK', json=CCISHORT)  
       Tb.telegram_send_message( "🎱 " + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n⚠️ No Operar \nBOT TENDENCIA")

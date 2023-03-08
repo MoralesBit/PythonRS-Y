cee5a3d20fd80b2e0af2e9b2c3a7a22d3e145ec1
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-08 14:31:36 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -58,9 +58,9 @@ def indicator(symbol):
   rsi = ta.RSI(df["Close"], timeperiod=period)
 
   
-  BBtop = round((float(df['Close'][-2]) - df['lowerband'][-2]),2)
-  BBdown = round((df['upperband'][-2] - df['lowerband'][-2]),2)
-  
+  BBtop = (float(df['Close'][-2]) - df['lowerband'][-2])
+  BBdown = (df['upperband'][-2] - df['lowerband'][-2])
+  BB = round((BBtop/BBdown),2)
 
   info = client.futures_historical_klines("BTCUSDT", "15m", "24 hours ago UTC+1",limit=1000) 
   df_new = pd.DataFrame(info)
@@ -90,7 +90,7 @@ def indicator(symbol):
   #tra = ta.TRANGE(df['High'], df['Low'], df['Close'])
   
   print(symbol)
-  print(BBtop/BBdown)
+  print(BB)
          
   CCISHORT = {
   "name": "CCI SHORT",
@@ -131,17 +131,14 @@ def indicator(symbol):
       #Tb.telegram_canal_prueba( "EMA 13-100: \n" + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n EMA 13 " + str(round((df['EMA13'][-1]),3)) + "\n EMA 100: " + str(round((df['EMA100'][-1]),3)))
   
   # fishing Pisha    
-  if BBtop % BBdown == 0:
+  if BB == 0:
         print ("marico el que lo lee")
   else: 
-    if (float(BBtop/BBdown) <= 0 ) and (cci5[-2] < 0) and (cci5[-1] > 0):
+    if (BB <= 0) and (cci5[-2] < 0) and (cci5[-1] > 0):
         requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=PLONG)
         Tb.telegram_send_message( "⚡️ " + symbol + "\n🟢 LONG \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n📶 BB : " + str(BBtop/BBdown) + " \n⚠️ No Operar \n📈 Fishing Pisha")
   
-  if BBtop % BBdown == 0:
-        print ("marico el que lo lee")   
-  else:
-    if (float(BBtop/BBdown) >= 1) and (cci5[-2] > 0) and (cci5[-1] < 0):
+    if (BB >= 1) and (cci5[-2] > 0) and (cci5[-1] < 0):
       requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=PSHORT)  
       Tb.telegram_send_message( "⚡️ " + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n📶 BB : " + str(BBtop/BBdown)+ "\n⚠️ No Operar \n📉 Fishing Pisha")
   

211aef01dd3f0b42856f8c239d51e8f2247e18a5
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-12 15:28:54 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -46,6 +46,7 @@ def indicator(symbol):
                                     signalperiod=9)
   cci3 = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=3) 
   cci5 = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=5)
+  cci20 = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=20)
   cci28 = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=28)
   cci58 = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=58)
   
@@ -149,11 +150,11 @@ def indicator(symbol):
   print(float(df['Close'][-2]))
   
   # Fishing Pisha Nuevo 
-  if (rsi[-1] > 70) and (cci3[-2] < 0) and (cci3[-1] > 0):
+  if (rsi[-2] < adx[-1]) and (cci20[-2] > 100) and (cci20[-1] < 100):
       requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=PLONG)
       Tb.telegram_send_message( "⚡️ " + symbol + "\n🟢 LONG \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n🎣 Fishing Pisha")
   
-  if (rsi[-1] < 30) and (cci3[-2] > 0) and (cci3[-1] < 0):
+  if (rsi[-2] > adx[-1]) and (cci20[-2] < -100) and (cci20[-1] > -100):
       requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=PSHORT)  
       Tb.telegram_send_message( "⚡️ " + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n🎣 Fishing Pisha")
       

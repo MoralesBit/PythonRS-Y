ab93abe50a353a996941eabd4c5af074e0233a5f
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-13 10:46:53 -0400

    Update Pumper.py

--- Pumper.py
+++ Pumper.py
@@ -111,11 +111,11 @@ def indicator(symbol):
   if (diff > 1):
     if (rsi[-2] > 30) and (adx[-2] < adx[-1]) and (cci3[-2] < 0) and (cci3[-1] > 0):    
       requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=UNOLONG)
-      Tb.telegram_canal_3por( "⚡️ " + symbol + "\n🟢 LONG \n⏳ 3min \n💵 Precio: " + df['Close'][-1] + "\n🔝  Cambio: " + str(diff) + " %" + "\n📈  RSI: " + str(rsi[-1]))
+      Tb.telegram_canal_prueba( "⚡️ " + symbol + "\n🟢 LONG \n⏳ 3min \n💵 Precio: " + df['Close'][-1] + "\n🔝  Cambio: " + str(diff) + " %" + "\n📈  RSI: " + str(rsi[-1]))
   if (diff > 1):
     if (rsi[-2] < 70) and (adx[-2] > adx[-1]) and (cci3[-2] > 0) and (cci3[-1] < 0):   
       requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=UNOSHORT)  
-      Tb.telegram_canal_3por( "⚡️ " + symbol + "\n🔴 SHORT \n⏳ 3min \n💵 Precio: " + df['Close'][-1] + "\n🔝  Cambio: " + str(diff) + " %" + "\n📉  RSI: " + str(rsi[-1]))
+      Tb.telegram_canal_prueba( "⚡️ " + symbol + "\n🔴 SHORT \n⏳ 3min \n💵 Precio: " + df['Close'][-1] + "\n🔝  Cambio: " + str(diff) + " %" + "\n📉  RSI: " + str(rsi[-1]))
   
   
   return round(last_rsi, 1), rsi_stat

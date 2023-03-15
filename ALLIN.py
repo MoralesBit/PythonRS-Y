38a0d85036608b2a9feb431e70bf97f516443913
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-15 19:03:55 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -115,24 +115,16 @@ def indicator(symbol):
   "symbol": symbol
   }
          
-  #Bouncy
-  if (cci14[-2] > 100) and (cci14[-1] < 100) and (rsi4[-1] > 60):
-      #requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=PLONG)
-      Tb.telegram_canal_prueba( "⚡️ " + symbol + "\n🟢 LONG \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\nBOUNCY")
-  
-  if (cci14[-2] < -100) and (cci14[-1] > -100) and (rsi4[-1] < 40):
-      Tb.telegram_canal_prueba( "⚡️ " + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\nBOUNCY")
-  
-  
-  # Fishing Pisha Nuevo 
   #LONG FISHING
   
-  if (cci14[-2] > 100) and (cci14[-1] < 100) and (rsi4[-1] > 60):    
+  if (df['tendencia'][-1] == 1):
+    if (cci14[-2] < 0) and (cci14[-1] > 0) and (rsi4[-1] > 70):      
       requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=PLONG)
       Tb.telegram_send_message( "⚡️ " + symbol + "\n🟢 LONG \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n🎣 Fishing Pisha")
   
   #SHORT FISHING
-  if (cci14[-2] < -100) and (cci14[-1] > -100) and (rsi4[-1] < 40):  
+  if (df['tendencia'][-1] == 0):
+    if (cci14[-2] > 0) and (cci14[-1] < 0) and (rsi4[-1] < 30):  
       requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=PSHORT)  
       Tb.telegram_send_message( "⚡️ " + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n🎣 Fishing Pisha")
   

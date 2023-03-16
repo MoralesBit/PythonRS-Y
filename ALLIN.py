3b6c0252b809e7e326140ee11e344275db65d2be
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-16 16:10:03 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -83,12 +83,11 @@ def indicator(symbol):
                                     signalperiod=9)   
   
   df_new['EMA200'] = df_new['Close'].ewm(200).mean()
+  
   Close = float(df_new['Close'][-1])
-  Close2 = float(df_new['Close'][-2])
+  High = float(df_new['High'][-1])
+  Low = float(df_new['Low'][-1])
   Open = float(df_new['Open'][-1])
-  Open2 = float(df_new['Open'][-2])
-  Low2 = float(df_new['Low'][-2])
-  High2 = float(df_new['High'][-2])
     
     
   print(symbol)
@@ -124,13 +123,13 @@ def indicator(symbol):
          
   #LONG FISHING
   
-  if (histB[-3] < histB[-2]):
+  if (histB[-3] < histB[-2]) and (Close >= Open):
     if (cci3[-3] < 0) and (cci14[-2] > 0) and (rsi4[-3] > 70 > rsi4[-2]):      
       requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=PLONG)
       Tb.telegram_send_message( "⚡️ " + symbol + "\n🟢 LONG \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n🎣 Fishing Pisha")
   
   #SHORT FISHING
-  if (histB[-3] > histB[-2]):
+  if (histB[-3] > histB[-2]) and (Close <= Open):
     if (cci3[-3] > 0) and (cci14[-2] < 0) and (rsi4[-3] < 30 < rsi[-2]):  
       requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=PSHORT)  
       Tb.telegram_send_message( "⚡️ " + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n🎣 Fishing Pisha")
@@ -155,4 +154,4 @@ schedule.every(5).minutes.at(":01").do(server_time)
 while True:
     #server_time()
     schedule.run_pending()
-    ti.sleep(1)   
+    ti.sleep(5)   

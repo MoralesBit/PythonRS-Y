e35570d9f1c9dd66e4ba050a2e1daa686420646a
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-19 10:12:22 -0400

    Actualizar Pumper.py

--- Pumper.py
+++ Pumper.py
@@ -76,11 +76,11 @@ def indicator(symbol):
   "symbol": symbol
   }
   
-  if (macdB[-2] > signal[-2]): 
+  if (macdB[-2] > signalB[-2]): 
     if (cci20[-3] < 0) and (cci20[-2] > 0):    
       requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=UNOLONG)
       Tb.telegram_send_message(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📈  Fast Trend")
-  if (macdB[-2] < signal[-2]): 
+  if (macdB[-2] < signalB[-2]): 
     if (cci20[-3] > 0) and (cci20[-2] < 0):   
       requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=UNOSHORT)  
       Tb.telegram_send_message(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📉  Fast Trend")

cf6bb321feafd14d7232abf95b95825c476bc5fc
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-20 09:33:09 -0400

    Update Pumper.py

--- Pumper.py
+++ Pumper.py
@@ -78,11 +78,11 @@ def indicator(symbol):
   }
   
   if (macdB[-2] > signalB[-2]) and (macdB[-3] < macdB[-2]): 
-    if (cci20[-3] < 0) and (cci20[-2] > 0) and (adx[-3] < adx[-2]):    
+    if (cci20[-3] < 0) and (cci20[-2] > 0) and (adx[-3] < adx[-2]) and (df['macd_hist'][-3] < df['macd_hist'][-2]):    
       requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=UNOLONG)
       Tb.telegram_send_message(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📈  Fast Trend")
   if (macdB[-2] < signalB[-2]) and (macdB[-3] > macdB[-2]): 
-    if (cci20[-3] > 0) and (cci20[-2] < 0) and (adx[-3] < adx[-2]):   
+    if (cci20[-3] > 0) and (cci20[-2] < 0) and (adx[-3] < adx[-2]) and (df['macd_hist'][-3] > df['macd_hist'][-2]):   
       requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=UNOSHORT)  
       Tb.telegram_send_message(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📉  Fast Trend")
   

e26f7998f29ce483faf51aa3230fa867d9d9e278
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-26 10:35:42 -0400

    Update Pumper.py

--- Pumper.py
+++ Pumper.py
@@ -98,7 +98,7 @@ def indicator(symbol):
   
   #SHORT FISHING en 3 min
   
-  if (cci28[-3] > 0) and (cci28[-2] < 0) and (macd[-2] < signal[-2]):
+  if (cci20[-3] > 0) and (cci20[-2] < 0) and (macd[-2] < signal[-2]):
     if (adxr[-2] > 25)  and (cciB[-2] < -50):  
       #requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=PSHORT)  
       Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📉  Mini FIshing")  
@@ -115,11 +115,11 @@ def indicator(symbol):
       #Tb.telegram_send_message(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📉  Fast Trend")
   
   #25/03/2023: 
-  if cciB58[-2] > 40:
+  if cciB58[-2] > 50:
    if (df['macd'][-3] <  df['macd_signal'][-3]) and (df['macd'][-2] > df['macd_signal'][-2]) and (adx[-2] > 20) and (chain[-2] > 100000):      
       #requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=PLONG)
       Tb.telegram_send_message(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📈  Fast Trend")
-  if  cciB58[-2] < -40:
+  if  cciB58[-2] < -50:
    if (df['macd'][-3] >  df['macd_signal'][-3]) and (df['macd'][-2] < df['macd_signal'][-2]) and (adx[-2] > 20) and (chain[-2] > -100000):
       #requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=PSHORT) 
       Tb.telegram_send_message(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📉  Fast Trend")

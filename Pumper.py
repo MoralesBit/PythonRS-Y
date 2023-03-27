781da7957074c5103d045a656052661d97f7d8f3
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-27 11:01:56 -0400

    Update Pumper.py

--- Pumper.py
+++ Pumper.py
@@ -100,9 +100,7 @@ def indicator(symbol):
     
   #LONG Y SHORT > 50 - MINI FISHING 
   if (df['tendenciaB'][2] == 0): 
-    if (cci20[-3] < 0) and (cci20[-2] > 0) and (macd[-2] > signal[-2]) and (adxr[-2] > 25):     
-      #requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=PLONG)
-      Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📈  Mini FIshing")
+   
     if (cci28[-3] > 0) and (cci28[-2] < 0) and (macd[-2] < signal[-2]) and (adxr[-2] > 25):  
       #requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=PSHORT)  
       Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3min\n💵 Precio: {df['Close'][-1]}\n📉  Mini FIshing")

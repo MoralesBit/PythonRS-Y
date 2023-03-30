e4a19b81aef8acd3c5801d507e385fe6766dc186
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-03-30 11:57:29 -0400

    Update ALLIN.py

--- ALLIN.py
+++ ALLIN.py
@@ -132,14 +132,14 @@ def indicator(symbol):
   #LONG FISHING
   
   if (cciB58[-3] < 0) and (cciB58[-2] > 0) and (macdB[-2] > signalB[-2]):
-    if (adxr[-2] > 25):      
+    if (adxr[-2] > 35):      
       requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=PLONG)
       Tb.telegram_send_message( "⚡️ " + symbol + "\n🟢 LONG \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n🎣 Fishing Pisha")
   
   #SHORT FISHING
   
   if (cciB58[-3] > 0) and (cciB58[-2] < 0) and (macdB[-2] < signalB[-2]):
-    if (adxr[-2] > 25):  
+    if (adxr[-2] > 35):  
       requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=PSHORT)  
       Tb.telegram_send_message( "⚡️ " + symbol + "\n🔴 SHORT \n⏳ 15min \n💵 Precio: " + df['Close'][-1] + "\n🎣 Fishing Pisha")
   

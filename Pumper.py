a272e9bad7d627fea61aa393a4ca21c60923d06c
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-06 10:39:31 -0400

    Update Pumper.py

--- Pumper.py
+++ Pumper.py
@@ -126,18 +126,18 @@ while True:
       # Chequea si el precio es mayor al canal más alto de Fibonacci y si hay un cruce bajista de MACD y Signal o un cruce bajista del RSI y el nivel 70
       
       # Contra-Tendencia (Cierre de la tendencia)
-      if (prices[-1] > fourth_level) and (rsi[-2] > 70 and rsi[-1] < 70) and (adx[-1] > 20) and (adx[-1] < 40):
+      if (prices[-1] > fourth_level) and (rsi[-2] > 70 and rsi[-1] < 70):
         Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 5 min\n💵 Precio: {prices[-1]}\n💰 P-Max: {round(fourth_level,4)}\n Contratendencia ")
         requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=CONTRASHORT)   
-      if (prices[-1] < first_level) and (rsi[-2] < 30 and rsi[-1] > 30) and (adx[-1] > 20) and (adx[-1] < 40):
+      if (prices[-1] < first_level) and (rsi[-2] < 30 and rsi[-1] > 30):
         Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 5 min\n💵 Precio: {prices[-1]}\n💰 P-Min: {round(first_level,4)}\n Contratendencia")
         requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=CONTRALONG) 
         
       #Tendencia FISHING
-      if (ema > prices[-2] < third_level < prices[-1]) and (macd[-1] < signal[-1] and macd[-2] > signal[-2]) and (adx[-1] > 20) and (adx[-1] < 40):
+      if (ema > prices[-2] < third_level < prices[-1]) and (macd[-1] < signal[-1] and macd[-2] > signal[-2]):
         Tb.telegram_send_message(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 5 min\n💵 Precio: {prices[-1]}\n💰 P-Min: {round(fourth_level,4)}\n🎣 Fishing Pisha") 
         requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=FISHINGSHORT) 
-      if (ema < prices[-2] > third_level > prices[-1]) and (macd[-1] > signal[-1] and macd[-2] < signal[-2]) and (adx[-1] > 20) and (adx[-1] < 40):
+      if (ema < prices[-2] > third_level > prices[-1]) and (macd[-1] > signal[-1] and macd[-2] < signal[-2]):
         Tb.telegram_send_message(f"⚡️ {symbol}\n🟢 LONG\n⏳ 5 min\n💵 Precio: {prices[-1]}\n💰 P-Max: {round(first_level,4)}\n🎣 Fishing Pisha") 
         requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=FISHINGLONG) 
         
@@ -145,4 +145,5 @@ while True:
       
       # Imprime los resultados
 
-      print(symbol)
+      print(symbol)    
+   

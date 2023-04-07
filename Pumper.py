252b31bd60c81483ee92239adaf0de3a8c8d19b1
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-06 21:33:16 -0400

    Update Pumper.py

--- Pumper.py
+++ Pumper.py
@@ -140,10 +140,10 @@ while True:
         requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=CONTRALONG) 
         
       #Tendencia FISHING
-      if (prices[-2] < fourth_level) and (macd[-2] < signal[-2] and macd[-3] > signal[-3]):
+      if (ema > prices[-2] < fourth_level) and (macd[-2] < signal[-2] and macd[-3] > signal[-3]):
         Tb.telegram_send_message(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 5 min\n💵 Precio: {prices[-1]}\n💰 P-Min: {round(fourth_level,4)}\n🎣 Fishing Pisha") 
         requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=FISHINGSHORT) 
-      if (prices[-2] > first_level) and (macd[-2] > signal[-2] and macd[-3] < signal[-3]):
+      if (ema < prices[-2] > first_level) and (macd[-2] > signal[-2] and macd[-3] < signal[-3]):
         Tb.telegram_send_message(f"⚡️ {symbol}\n🟢 LONG\n⏳ 5 min\n💵 Precio: {prices[-1]}\n💰 P-Max: {round(first_level,4)}\n🎣 Fishing Pisha") 
         requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=FISHINGLONG) 
         

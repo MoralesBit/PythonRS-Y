deb453d08d5f265c2d8604567ceff815eac2a98c
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-08 15:24:46 -0400

    Update Pumper.py

--- Pumper.py
+++ Pumper.py
@@ -179,7 +179,7 @@ if __name__ == '__main__':
 while True:
     # Espera hasta que sea el comienzo de una nueva hora
     current_time = ti.time()
-    seconds_to_wait = 180 - current_time % 180
+    seconds_to_wait = 300 - current_time % 300
     ti.sleep(seconds_to_wait)   
   
     for symbol in symbols:

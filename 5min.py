4e80c130ac77669839717c38c6c6c6712532dce3
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-09 14:54:24 -0400

    Update 5min.py

--- 5min.py
+++ 5min.py
@@ -164,10 +164,10 @@ def indicator(symbol):
           requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=CONTRALONG) 
         
       #Tendencia FISHING
-        if (df['EMA200'][-2] < Close) and (float(df['Close'][-2]) < df['third_level'][-2]) and (df['third_level'][-3]) < (float(df['Close'][-3])) and (adx[-2] > 25):
+        if (df['EMA200'][-2] > Close) and (float(df['Close'][-2]) < df['fourth_level'][-2]) and (df['fourth_level'][-3]) < (float(df['Close'][-3])) and (adx[-2] > 40):
           Tb.telegram_send_message(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 5 min\n💵 Precio: {Close}\n Fb 0.5 : {round(df['third_level'][-2],2)}\n🎣 Fishing Pisha") 
           requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=FISHINGSHORT) 
-        if (df['EMA200'][-2] > Close ) and (float(df['Close'][-2]) > df['third_level'][-2]) and (df['third_level'][-3]) > (float(df['Close'][-3])) and (adx[-2] > 25):
+        if (df['EMA200'][-2] < Close ) and (float(df['Close'][-2]) > df['fourth_level'][-2]) and (df['fourth_level'][-3]) > (float(df['Close'][-3])) and (adx[-2] < 20):
           Tb.telegram_send_message(f"⚡️ {symbol}\n🟢 LONG\n⏳ 5 min\n💵 Precio: {Close}\n Fb 0.5 : {round(df['third_level'][-2],2)}\n🎣 Fishing Pisha") 
           requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=FISHINGLONG) 
         

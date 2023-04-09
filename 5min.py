d054c87bf6e2d49dca331dc7e124b93eb4c87ce4
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-09 12:21:57 -0400

    Update 5min.py

--- 5min.py
+++ 5min.py
@@ -8,23 +8,12 @@ import time as ti
 import requests
 import json
 
-api_key = 'TU_API_KEY'
-api_secret = 'TU_API_SECRET'
+Pkey = ''
+Skey = ''
 
-client = Client(api_key, api_secret)
+client = Client(api_key=Pkey, api_secret=Skey)
 
-futures_info = client.futures_exchange_info()
-symbols = [symbol['symbol'] for symbol in futures_info['symbols']]
-
-while True:
-    # Espera hasta que sea el comienzo de una nueva hora
-    current_time = ti.time()
-    seconds_to_wait = 300 - current_time % 300
-    ti.sleep(seconds_to_wait)   
-  
-    for symbol in symbols:
-      def indicator(symbol):
-    
+def indicator(symbol):   
         kline = client.futures_historical_klines(symbol, "5m", "12 hours ago UTC+1",limit=500)
         df = pd.read_json(json.dumps(kline))
     
@@ -178,6 +167,15 @@ while True:
           requests.post('https://hook.finandy.com/VMfD-y_3G5EgI5DUqFUK', json=VIEWLONG)
           
            
-      # Imprime los resultados
+futures_info = client.futures_exchange_info()
+symbols = [symbol['symbol'] for symbol in futures_info['symbols']]
 
-      print(symbol)    
+while True:
+  # Espera hasta que sea el comienzo de una nueva hora
+  current_time = ti.time()
+  seconds_to_wait = 180 - current_time % 180
+  ti.sleep(seconds_to_wait)   
+  
+  for symbol in symbols:
+    indicator(symbol)
+    print(symbol) 

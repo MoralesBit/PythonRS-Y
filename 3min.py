ae6a783f907edf00c683db6c151d58b5f955d6c8
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-10 09:11:25 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -112,10 +112,10 @@ def indicator(symbol):
     }
      
    
-  if (diff > 1) and (rsi[-2] > 80) and (Close > df['upperband'][-2]):
+  if (diff > 1) and (rsi[-2] > 70) and (Close > df['upperband'][-2]):
     Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close} \n⛳️ Snipper : {max_ask} ") 
     requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT) 
-  if (diff > 1) and (rsi[-2] < 20) and (Close < df['lowerband'][-2]):
+  if (diff > 1) and (rsi[-2] < 30) and (Close < df['lowerband'][-2]):
     Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close} \n⛳️ Snipper : {max_bid} ")
     requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)
     

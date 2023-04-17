03fae59c3905ed3a29bc21bd4d930f66cb346971
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-17 11:36:44 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -74,27 +74,20 @@ def indicator(symbol):
 
     response = requests.get(f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit={depth}').json()
 
-    if 'bids' in response:
-          bid_sum = sum([float(bid[1]) for bid in response['bids']])
-    else:
-          bid_sum = 0.0
+    bid_sum = sum([float(bid[1]) for bid in response['bids']])
+    ask_sum = sum([float(ask[1]) for ask in response['asks']])
+    bid_max = max([float(bid[1]) for bid in response['bids']])
+    ask_max = max([float(ask[1]) for ask in response['asks']])
 
-    if 'asks' in response:
-          ask_sum = sum([float(ask[1]) for ask in response['asks']])
-    else:
-          ask_sum = 0.0
-
-    if bid_sum + ask_sum > 0:
-          imbalance = (ask_sum - bid_sum) / (bid_sum + ask_sum)
-    else:
-          imbalance = 0.0  
-        
+    imbalance = (ask_sum - bid_sum) / (bid_sum + ask_sum) 
+    print(imbalance)
+    
   #Actual   
-  if (diff > 1) and (Close > upperband[-2]) and (rsi[-2] > 70) and (slowk[-2] > 95) and (imbalance < 0):
-    Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close} \n⛳️ Snipper : {float(df['Close'][-2])} ") 
+  if (diff > 1) and (Close > upperband[-2]) and (rsi[-2] > 70) and (adx[-2] >= 40) and (slowk[-2] > 95):
+    Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)} \n⛳️ Snipper : {ask_max} ") 
     requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT) 
-  if (diff > 1) and (Close < lowerband[-2]) and (rsi[-2] < 30) and (slowk[-2] < 5) and (imbalance > 0):
-    Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close} \n⛳️ Snipper : {float(df['Close'][-2])} ")
+  if (diff > 1) and (Close < lowerband[-2]) and (rsi[-2] < 30) and (adx[-2] <= 20) and (slowk[-2] < 5):
+    Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)} \n⛳️ Snipper : {bid_max} ")
     requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)
     
       
@@ -102,7 +95,7 @@ while True:
     # Espera hasta que sea el comienzo de una nueva hora
     current_time = ti.time()
     seconds_to_wait = 180 - current_time % 180
-    ti.sleep(seconds_to_wait)   
+    #ti.sleep(seconds_to_wait)   
   
     for symbol in symbols:
       indicator(symbol)

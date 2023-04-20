28470c5d85adad82f54254c0a53fea359c126f56
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-19 22:59:32 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -1,143 +1,107 @@
 from binance.client import Client
-import pandas as pd
 import numpy as np
-import talib as ta
+import pandas as pd
+import talib
 import Telegram_bot as Tb
-import  schedule as schedule
-import time as ti
+import time
 import requests
-import json
 
-Pkey = ''
-Skey = ''
 
-client = Client(api_key=Pkey, api_secret=Skey)
+api_key = 'TU_API_KEY'
+api_secret = 'TU_API_SECRET'
 
-monedas = client.futures_exchange_info()
-  # 1. Obtener todas las monedas tradeables de futuros
-symbols = [
-    symbol['symbol'] for symbol in monedas['symbols']
-    if symbol['status'] == "TRADING"
-  ]
-#symbols = ["BLZUSDT", "ARUSDT", "INJUSDT", "STORJUSDT","HNTUSDT", "ARPAUSDT"]
+client = Client(api_key, api_secret)
 
-def indicator(symbol):
-  
-  kline = client.futures_historical_klines(symbol, "3m", "2 days ago UTC+1",limit=1000)
-  df = pd.read_json(json.dumps(kline))
-  
-  if not df.empty:
-    df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close' 'IGNORE',
-      'Quote_Volume', 'Trades_Count', 'BUY_VOL', 'BUY_VOL_VAL', 'x']
-    df['Date'] = pd.to_datetime(df['Date'], unit='ms')
-    df = df.set_index('Date')
-    
-   
-  upperband, middleband, lowerband = ta.BBANDS(df['Close'],
-                                               timeperiod=20,
-                                               nbdevup=2,
-                                               nbdevdn=2,
-                                               matype=0)
-  macd, signal, hist = ta.MACD(df['Close'], 
-                                    fastperiod=12, 
-                                    slowperiod=26, 
-                                    signalperiod=9)
-  df['macd'] = macd
-  df['macd_signal'] = signal
-  df['macd_hist'] = hist
-  df['macd_crossover'] = np.where(df['macd'] > df['macd_signal'], 1, 0)
-  df['position_macd'] = df['macd_crossover'].diff()
-  
-  rsi = ta.RSI(df["Close"], timeperiod=4)
-  df['EMA100'] = df['Close'].ewm(100).mean()
-  df['tendencia'] = np.where((float(df['Close'][-1])) > (df['EMA100'][-1]), 1,0)
-  
-  info = client.futures_historical_klines("BTCUSDT", "15m", "2 days ago UTC+1",limit=1000) 
-  df_new = pd.DataFrame(info)
+futures_info = client.futures_exchange_info()
+symbols = [symbol['symbol'] for symbol in futures_info['symbols']]
+
+# Crea una función para generar los canales de Fibonacci:
        
-  if not df_new.empty:
-    df_new.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close' 'IGNORE',
-      'Quote_Volume', 'Trades_Count', 'BUY_VOL', 'BUY_VOL_VAL', 'x']
-    df_new['Date'] = pd.to_datetime(df_new['Date'], unit='ms')
-    df_new = df_new.set_index('Date')
-  
-    upperband, middleband, lowerband = ta.BBANDS(df['Close'],
-                                                timeperiod=20,
-                                                nbdevup=2,
-                                                nbdevdn=2,
-                                                matype=0)
-    df['upperband'] = upperband
-    df['middleband'] = middleband
-    df['lowerband'] = lowerband
-       
-    rsi = ta.RSI(df["Close"], timeperiod=14)
-    adx = ta.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
-    slowk, slowd = ta.STOCH(df['High'], df['Low'], df['Close'], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
-    
-    Close = float(df['Close'][-2])
-    Open = float(df['Open'][-2])
-    High = float(df['High'][-2])
-    Low = float(df['Low'][-2])
-    diff = abs((High / Low -1) * 100)  
+def calculate_adx(prices_high, prices_low, prices_close):
+    adx = talib.ADX(prices_high, prices_low, prices_close, timeperiod=14)
+    return adx 
 
-    
-    PORSHORT = {
+
+def calculate_bbands(prices):
+    upper, middle, lower = talib.BBANDS(prices, timeperiod=20, nbdevup=2, nbdevdn=2, matype=talib.MA_Type.SMA)
+    return upper, middle, lower  
+
+
+  
+while True:
+    # Espera hasta que sea el comienzo de una nueva hora
+    current_time = time.time()
+    seconds_to_wait = 300 - current_time % 300
+    time.sleep(seconds_to_wait)   
+  
+    for symbol in symbols:
+         
+    # Obtén los datos de precios históricos para el símbolo
+      klines = client.futures_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_3MINUTE)
+          
+      prices = np.array([float(kline[2]) for kline in klines])
+      prices_high = np.array([float(kline[3]) for kline in klines])
+      prices_low = np.array([float(kline[4]) for kline in klines])
+      prices_close = np.array([float(kline[5]) for kline in klines])
+      
+      diff = abs((prices_high / prices_low -1) * 100) 
+         
+         
+      # Calcula el indicador RSI
+      rsi = talib.RSI(prices, timeperiod=14)
+             
+      # Calcula el indicador ADX
+      adx = calculate_adx(prices_high, prices_low, prices_close)
+      
+       # Calcula Bandas de Bollinger
+      upperband, middleband, lowerband = calculate_bbands(prices_close)
+      
+      depth = 5
+
+      response = requests.get(f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit={depth}').json()
+      if 'bids' in response:
+          bid_sum = sum([float(bid[1]) for bid in response['bids']])
+      else:
+          bid_sum = 0.0
+
+      if 'asks' in response:
+          ask_sum = sum([float(ask[1]) for ask in response['asks']])
+      else:
+         ask_sum = 0.0
+
+      if bid_sum + ask_sum > 0:
+          imbalance = (ask_sum - bid_sum) / (bid_sum + ask_sum)
+      else:
+          imbalance = 0.0      
+      
+      PORSHORT = {
     "name": "CORTO 3POR",
     "secret": "ao2cgree8fp",
     "side": "sell",
     "symbol": symbol,
     "open": {
-    "price": float(df['Close'][-2])
+    "price": prices_close[-2]
     }
     }
-    PORLONG = {
+      PORLONG = {
     "name": "LARGO 3POR",
     "secret": "nwh2tbpay1r",
     "side": "buy",
     "symbol": symbol,
     "open": {
-    "price": float(df['Close'][-2])
+    "price": prices_close[-2]
     }
     }
-    
-    url = f'https://api.binance.com/api/v3/depth?symbol=BTCUSDT&limit=10'
-    response = requests.get(url).json() 
-    bids = response['bids']
-    asks = response['asks']
-    
-    bid_sum = sum([float(bid[1]) for bid in bids])
-    ask_sum = sum([float(ask[1]) for ask in asks])
-   
-   
-    if 'bids' in response:
-          bid_sum = sum([float(bid[1]) for bid in response['bids']])
-    else:
-      bid_sum = 0.0
+      # Chequea si el precio es mayor al canal más alto de Fibonacci y si hay un cruce bajista de MACD y Signal o un cruce bajista del RSI y el nivel 70
+      
+      #Actual   
+      if (diff[-2] > 1) and (prices_close[-2] > upperband[-2]) and (rsi[-2] > 70)and (imbalance < -0.6):
+        Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff[-2],2)} \n💵 Precio: {prices_close[-2]}\n IMB: {round(imbalance,2)}") 
+        requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT) 
+      if (diff[-2] > 1) and (prices_close[-2] < lowerband[-2]) and (rsi[-2] < 30) and (imbalance > 0.6):
+        Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff[-2],2)} \n💵 Precio: {prices_close[-2]}\n IMB: {round(imbalance,2)}")
+        requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)
+      
+      # Imprime los resultados
 
-    if 'asks' in response:
-      ask_sum = sum([float(ask[1]) for ask in response['asks']])
-    else:
-      ask_sum = 0.0
-
-    if bid_sum + ask_sum > 0:
-      imbalance = (ask_sum - bid_sum) / (bid_sum + ask_sum)
-    else:
-      imbalance = 0.0 
-     
- #Actual   
-  if (diff > 1) and (Close > upperband[-2]) and (rsi[-2] > 70)and (imbalance < -0.7) and (adx[-2] >= 40):
-    Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)}") 
-    requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT) 
-  if (diff > 1) and (Close < lowerband[-2]) and (rsi[-2] < 30) and (imbalance > 0.7) and (adx[-2] <= 20):
-    Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)}")
-    requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)    
-       
-while True:
-      # Espera hasta que sea el comienzo de una nueva hora
-  current_time = ti.time()
-  seconds_to_wait = 300 - current_time % 300
-  ti.sleep(seconds_to_wait)   
-  
-  for symbol in symbols:
-    indicator(symbol)
-    print(symbol)
+      print(symbol)

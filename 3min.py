49dbab9e05d1052e3279d981a80a6fae5a320ab2
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-19 22:10:44 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -1,31 +1,66 @@
-import requests
+from binance.client import Client
 import pandas as pd
 import numpy as np
 import talib as ta
 import Telegram_bot as Tb
-import schedule as schedule
+import  schedule as schedule
 import time as ti
+import requests
 import json
 
-api_key = 'TU_API_KEY'
-api_secret = 'TU_API_SECRET'
+Pkey = ''
+Skey = ''
 
-futures_info_url = 'https://fapi.binance.com/fapi/v1/exchangeInfo'
-response = requests.get(futures_info_url).json()
+client = Client(api_key=Pkey, api_secret=Skey)
+
+monedas = client.futures_exchange_info()
+  # 1. Obtener todas las monedas tradeables de futuros
 symbols = [
-    symbol['symbol'] for symbol in response['symbols']
+    symbol['symbol'] for symbol in monedas['symbols']
     if symbol['status'] == "TRADING"
   ]
+#symbols = ["BLZUSDT", "ARUSDT", "INJUSDT", "STORJUSDT","HNTUSDT", "ARPAUSDT"]
 
 def indicator(symbol):
-    
-  url = f'https://fapi.binance.com/fapi/v1/klines?symbol={symbol}&interval=3m&startTime=12 hours ago UTC+1&limit=500'
-  response = requests.get(url).json()
-  df = pd.DataFrame(response, columns=['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'])
-  df['Open time'] = pd.to_datetime(df['Open time'], unit='ms')
-  df = df.set_index('Open time')
+  
+  kline = client.futures_historical_klines(symbol, "3m", "2 days ago UTC+1",limit=1000)
+  df = pd.read_json(json.dumps(kline))
+  
   if not df.empty:
-
+    df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close' 'IGNORE',
+      'Quote_Volume', 'Trades_Count', 'BUY_VOL', 'BUY_VOL_VAL', 'x']
+    df['Date'] = pd.to_datetime(df['Date'], unit='ms')
+    df = df.set_index('Date')
+    
+   
+  upperband, middleband, lowerband = ta.BBANDS(df['Close'],
+                                               timeperiod=20,
+                                               nbdevup=2,
+                                               nbdevdn=2,
+                                               matype=0)
+  macd, signal, hist = ta.MACD(df['Close'], 
+                                    fastperiod=12, 
+                                    slowperiod=26, 
+                                    signalperiod=9)
+  df['macd'] = macd
+  df['macd_signal'] = signal
+  df['macd_hist'] = hist
+  df['macd_crossover'] = np.where(df['macd'] > df['macd_signal'], 1, 0)
+  df['position_macd'] = df['macd_crossover'].diff()
+  
+  rsi = ta.RSI(df["Close"], timeperiod=4)
+  df['EMA100'] = df['Close'].ewm(100).mean()
+  df['tendencia'] = np.where((float(df['Close'][-1])) > (df['EMA100'][-1]), 1,0)
+  
+  info = client.futures_historical_klines("BTCUSDT", "15m", "2 days ago UTC+1",limit=1000) 
+  df_new = pd.DataFrame(info)
+       
+  if not df_new.empty:
+    df_new.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close' 'IGNORE',
+      'Quote_Volume', 'Trades_Count', 'BUY_VOL', 'BUY_VOL_VAL', 'x']
+    df_new['Date'] = pd.to_datetime(df_new['Date'], unit='ms')
+    df_new = df_new.set_index('Date')
+  
     upperband, middleband, lowerband = ta.BBANDS(df['Close'],
                                                 timeperiod=20,
                                                 nbdevup=2,
@@ -69,19 +104,11 @@ def indicator(symbol):
     response = requests.get(url).json() 
     bids = response['bids']
     asks = response['asks']
-    max_bid = max([float(bid[0]) for bid in bids])
-    max_ask = max([float(ask[0]) for ask in asks])
+    
     bid_sum = sum([float(bid[1]) for bid in bids])
     ask_sum = sum([float(ask[1]) for ask in asks])
-    if bid_sum + ask_sum > 0:
-        imbalance_btc = (ask_sum - bid_sum) / (bid_sum + ask_sum)
-    else:
-        imbalance_btc = 0.0
-        
-    depth = 5
-
-    url = f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit={depth}'
-    response = requests.get(url).json()
+   
+   
     if 'bids' in response:
           bid_sum = sum([float(bid[1]) for bid in response['bids']])
     else:
@@ -96,22 +123,21 @@ def indicator(symbol):
       imbalance = (ask_sum - bid_sum) / (bid_sum + ask_sum)
     else:
       imbalance = 0.0 
-    
-  #Actual   
-  if (diff > 1) and (Close > upperband[-2]) and (rsi[-2] > 70)and (imbalance < -0.6):
+     
+ #Actual   
+  if (diff > 1) and (Close > upperband[-2]) and (rsi[-2] > 70)and (imbalance < -0.7) and (adx[-2] >= 40):
     Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)}") 
     requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT) 
-  if (diff > 1) and (Close < lowerband[-2]) and (rsi[-2] < 30) and (imbalance > 0.6):
+  if (diff > 1) and (Close < lowerband[-2]) and (rsi[-2] < 30) and (imbalance > 0.7) and (adx[-2] <= 20):
     Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)}")
-    requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)
-  
-  
+    requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)    
+       
 while True:
-    # Espera hasta que sea el comienzo de una nueva hora
-    current_time = ti.time()
-    seconds_to_wait = 180 - current_time % 180
-    ti.sleep(seconds_to_wait)   
+      # Espera hasta que sea el comienzo de una nueva hora
+  current_time = ti.time()
+  seconds_to_wait = 300 - current_time % 300
+  ti.sleep(seconds_to_wait)   
   
-    for symbol in symbols:
-      indicator(symbol)
-      print(symbol)
+  for symbol in symbols:
+    indicator(symbol)
+    print(symbol)

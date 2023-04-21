dafb8dcb0f48f2e37aa318944c41c55e184ef0d7
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-21 12:01:03 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -104,10 +104,10 @@ def indicator(symbol):
     if (diff > 1) and (Close < lowerband[-2]) and (rsi[-2] < 30) and (imbalance > 0.60):
         Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)}")
         requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG) 
-    if (cci20[-3] > 0) and (cci20[-2] < 0) and (imbalance < -0.25) and (spread_por > 1):
+    if (cci20[-3] > 0) and (cci20[-2] < 0) and (spread_por > 1):
         Tb.telegram_canal_prueba(f"C-I {symbol}\n🔴 SHORT\n⏳ 3 min\n💵 Precio: {Close}\nIMB : {round(imbalance,2)} C-I bid {max_bid} ask {max_ask}  " ) 
         requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=BOUNCYSHORT)     
-    if (cci20[-3] < 0) and (cci20[-2] > 0) and (imbalance > 0.25) and (spread_por > 1):
+    if (cci20[-3] < 0) and (cci20[-2] > 0) and (spread_por > 1):
         Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min\n💵 Precio: {Close}\nIMB : {round(imbalance,2)} C-I bid {max_bid} ask {max_ask}  ")
         requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=BOUNCYLONG) 
                

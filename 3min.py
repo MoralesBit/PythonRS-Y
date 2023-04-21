ea5e41733230155b45677dd67efc8768c21cf2c1
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-21 13:06:28 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -98,10 +98,10 @@ def indicator(symbol):
       # Chequea si el precio es mayor al canal más alto de Fibonacci y si hay un cruce bajista de MACD y Signal o un cruce bajista del RSI y el nivel 70
     
       #Actual   
-    if (diff > 1) and (Close > upperband[-2]) and (rsi[-2] >= 70) and (imbalance < -0.50) and (spread_por < 0.005):
+    if (rsi[-2] >= 70) and (imbalance < -0.50) and (spread_por < 0.005):
         Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)}") 
         requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT) 
-    if (diff > 1) and (Close < lowerband[-2]) and (rsi[-2] <= 30) and (imbalance > 0.50) and (spread_por > 0.005):
+    if (rsi[-2] <= 30) and (imbalance > 0.50) and (spread_por > 0.005):
         Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)}")
         requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG) 
     if (cci20[-3] > 0) and (cci20[-2] < 0) and (spread_por < 0.005):

4b9e436cd31e96797df1c32dd36e306e82997b0b
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-21 11:47:23 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -48,6 +48,7 @@ def indicator(symbol):
     ask_sum = sum([float(ask[1]) for ask in order_book['asks']])
     max_bid = float(order_book['bids'][0][0])
     max_ask = float(order_book['asks'][0][0])
+    spread = max_ask - max_bid  # Calcula el spread
  
     if bid_sum + ask_sum > 0:
      imbalance = (ask_sum - bid_sum) / (bid_sum + ask_sum)
@@ -73,19 +74,41 @@ def indicator(symbol):
     "price": max_bid
     }
     }
+    
+    BOUNCYSHORT= {
+  "name": "BOUNCY SHORT",
+  "secret": "hgw3399vhh",
+  "side": "sell",
+  "symbol": symbol,
+  "open": {
+    "price": max_ask
+  }
+}
+    BOUNCYLONG= {
+  "name": "BOUNCY LONG",
+  "secret": "xjth0i3qgb",
+  "side": "buy",
+  "symbol": symbol,
+  "open": {
+    "price": max_bid
+  }
+}
       # Chequea si el precio es mayor al canal más alto de Fibonacci y si hay un cruce bajista de MACD y Signal o un cruce bajista del RSI y el nivel 70
-      
+    print(spread)
       #Actual   
     if (diff > 1) and (Close > upperband[-2]) and (rsi[-2] > 70) and (imbalance < -0.60):
         Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)}") 
         requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT) 
     if (diff > 1) and (Close < lowerband[-2]) and (rsi[-2] < 30) and (imbalance > 0.60):
         Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n IMB: {round(imbalance,2)}")
-        requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)
-    if (cci20[-3] > 0) and (cci20[-2] < 0) and (imbalance < 0.05) and (imbalance > -0.05):
+        requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG) 
+    if (cci20[-3] > 0) and (cci20[-2] < 0) and (imbalance < -0.25) and (1 > spread > 0):
         Tb.telegram_canal_prueba(f"C-I {symbol}\n🔴 SHORT\n⏳ 3 min\n💵 Precio: {Close}\nIMB : {round(imbalance,2)} C-I bid {max_bid} ask {max_ask}  " ) 
-        requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT)     
-             
+        requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=BOUNCYSHORT)     
+    if (cci20[-3] < 0) and (cci20[-2] > 0) and (imbalance > 0.25) and (spread > 1):
+        Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min\n💵 Precio: {Close}\nIMB : {round(imbalance,2)} C-I bid {max_bid} ask {max_ask}  ")
+        requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=BOUNCYLONG) 
+               
 while True:
   current_time = ti.time()
   seconds_to_wait = 180 - current_time % 180

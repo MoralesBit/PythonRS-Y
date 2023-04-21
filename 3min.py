77f28cb6a48c262bb80a49c3a3fb32caeb82894c
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-20 22:20:17 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -17,9 +17,9 @@ symbols = [symbol['symbol'] for symbol in futures_info['symbols']]
 
 # Crea una función para generar los canales de Fibonacci:
        
-def calculate_adx(prices_high, prices_low, prices):
-    adx = talib.ADX(prices_high, prices_low, prices, timeperiod=14)
-    return adx 
+#def calculate_adx(prices_high, prices_low, prices):
+    #adx = talib.ADX(prices_high, prices_low, prices, timeperiod=14)
+    #return adx 
 
 
 def calculate_bbands(prices):
@@ -51,13 +51,13 @@ while True:
       rsi = talib.RSI(prices, timeperiod=14)
              
       # Calcula el indicador ADX
-      adx = calculate_adx(prices_high, prices_low, prices_close)
+      #adx = calculate_adx(prices_high, prices_low, prices_close)
       
        # Calcula Bandas de Bollinger
       upperband, middleband, lowerband = calculate_bbands(prices_close)
       
       #Imbalance
-      depth = 5
+      depth = 20
 
       response = requests.get(f'https://api.binance.com/api/v3/depth?symbol={symbol}&limit={depth}').json()
       if 'bids' in response:
@@ -96,10 +96,10 @@ while True:
       # Chequea si el precio es mayor al canal más alto de Fibonacci y si hay un cruce bajista de MACD y Signal o un cruce bajista del RSI y el nivel 70
       
       #Actual   
-      if (diff[-2] > 1) and (prices[-2] > upperband[-2]) and (rsi[-2] > 70)and (imbalance < -0.6):
+      if (diff[-2] > 1) and (prices[-2] > upperband[-2]) and (rsi[-2] > 70)and (imbalance < 0.10):
         Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff[-2],2)} \n💵 Precio: {prices[-2]}\n IMB: {round(imbalance,2)}") 
         requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT) 
-      if (diff[-2] > 1) and (prices[-2] < lowerband[-2]) and (rsi[-2] < 30) and (imbalance > 0.6):
+      if (diff[-2] > 1) and (prices[-2] < lowerband[-2]) and (rsi[-2] < 30) and (imbalance > -0.10):
         Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff[-2],2)} \n💵 Precio: {prices[-2]}\n IMB: {round(imbalance,2)}")
         requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)
       

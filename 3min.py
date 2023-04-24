a55e27f1eec3e494a8239edfbf02c1ee26f58732
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-24 16:09:08 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -123,13 +123,14 @@ def indicator(symbol):
     #Tendencia:     
     if (ema_200B[-2] > Close_B) and (imbalance < -0.50):
       if (60 < rsi[-2] < 70) or (40 < rsi[-2] < 50):
-        Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min\n💵 Precio: {Close} \n⛳️ Trend" ) 
-        requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=TRENDSHORT)
-        ti.sleep(0.1)      
+        Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min\n💵 Precio: {Close} \n⛳️ Trend")
+        requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=TRENDLONG)     
+             
     if (ema_200B[-2] < Close_B) and (imbalance > 0.50): 
       if (30 < rsi[-2] < 40) or (50 < rsi[-2] < 60):
-        Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min\n💵 Precio: {Close} \n⛳️ Trend")
-        requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=TRENDLONG) 
+        Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min\n💵 Precio: {Close} \n⛳️ Trend" ) 
+        requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=TRENDSHORT)
+        
        
                
 while True:

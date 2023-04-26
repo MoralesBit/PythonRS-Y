db74d84534ec64978c9ee50680e5b1716091ddeb
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-04-26 09:50:07 -0400

    Update 15min.py

--- 15min.py
+++ 15min.py
@@ -35,23 +35,23 @@ def indicator(symbol):
     High = float(df['High'][-2])
     Low = float(df['Low'][-2])
     diff = abs((High / Low -1) * 100)
-    #cci20 = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=20)
+#    cci20 = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=20)
     ema_200 = df['Close'].ewm(span=200, adjust=False).mean()
-    #adx = ta.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
-    ad = ta.AD(df['High'], df['Low'], df['Close'], df['Volume'])
-    df['Close'] = df['Close'].astype(float)
-    delta = df['Close'].diff()
-    fi = delta * ad
-    #macd, signal, hist = ta.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
-    slowk, slowd = ta.STOCH(df['High'], df['Low'], df['Close'], fastk_period=14, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+#    adx = ta.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
+#    ad = ta.AD(df['High'], df['Low'], df['Close'], df['Volume'])
+#    df['Close'] = df['Close'].astype(float)
+#    delta = df['Close'].diff()
+#    fi = delta * ad
+#    macd, signal, hist = ta.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
+#    slowk, slowd = ta.STOCH(df['High'], df['Low'], df['Close'], fastk_period=14, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
    
     
     
-    upperband, middleband, lowerband = ta.BBANDS(df['Close'],
-                                               timeperiod=20,
-                                               nbdevup=2,
-                                               nbdevdn=2,
-                                               matype=0)
+#    upperband, middleband, lowerband = ta.BBANDS(df['Close'],
+#                                               timeperiod=20,
+#                                               nbdevup=2,
+#                                               nbdevdn=2,
+#                                               matype=0)
     #depth = 5
     #order_book = client.futures_order_book(symbol=symbol, limit=depth)
 
@@ -87,23 +87,12 @@ def indicator(symbol):
     short = ma + ((ma / 100) *(0.8))
        
     # Enviar la solicitud a la API pública de Binance
-    response = requests.get(f'https://fapi.binance.com/fapi/v1/ticker/24hr?symbol={symbol}')
+#    response = requests.get(f'https://fapi.binance.com/fapi/v1/ticker/24hr?symbol={symbol}')
 
 # Obtener el volumen de negociación de las últimas 24 horas
-    volume = float(response.json()['volume'])
-    
-    depth = 5
-    order_book = client.futures_order_book(symbol=symbol, limit=depth)
+#    volume = float(response.json()['volume'])
 
-    bid_sum = sum([float(bid[1]) for bid in order_book['bids']])
-    ask_sum = sum([float(ask[1]) for ask in order_book['asks']])
-    max_bid = float(order_book['bids'][0][0])
-    max_ask = float(order_book['asks'][0][0])
- 
-    if bid_sum + ask_sum > 0:
-     imbalance = (ask_sum - bid_sum) / (bid_sum + ask_sum)
-    else:
-     imbalance = 0.0              
+
    # DATOS FNDY
   FISHINGSHORT = {
         "name": "FISHING SHORT",

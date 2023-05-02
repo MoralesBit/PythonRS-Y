6c6e95bbf5b9bd97478f78f3bbbdb5add26c2b3c
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-05-02 09:00:42 -0400

    Update 5min.py

--- 5min.py
+++ 5min.py
@@ -137,24 +137,24 @@ def indicator(symbol):
 }
        
 # Noro strategy:
-  if (Close > df['ema_660']):
+  if (Close > df['ema_660'][-2]):
     if (Close < long[-2]) and (rsi[-2] > 20) and (Close < middleband[-2]):
       Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n📍 Picker: {enter}") 
       requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=PICKERLONG)
       
-  if (Close < df['ema_660']):
+  if (Close < df['ema_660'][-2]):
    if (Close > short[-2]) and (rsi[-2] < 80) and (Close > middleband[-2]):
       Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n📍 Picker: {enter}")
       requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=PICKERSHORT)
       requests.post('https://hook.finandy.com/DRt05cAn8UjMWv5bqVUK', json=CARLOSSHORT) 
       
 # Tendencia:
-  if (Close < df['ema_660']):
+  if (Close < df['ema_660'][-2]):
     if (df['ema_200'][-3] < df['ema_13'][-3]) and (df['ema_200'][-2] > df['ema_13'][-2]) and (cci_20[-3] > cci_20[-2]):      
       Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n🏄🏻 KROSS")
       requests.post('https://hook.finandy.com/gZZtqWYCtUdF0WwyqFUK', json=FASTERSHORT)
   
-  if (Close > df['ema_660']):    
+  if (Close > df['ema_660'][-2]):    
     if (df['ema_200'][-3] > df['ema_13'][-3]) and (df['ema_200'][-2] < df['ema_13'][-2]) and (cci_20[-3] < cci_20[-2]): 
       Tb.telegram_canal_prueba(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(diff,2)} \n💵 Precio: {Close}\n🏄‍♂️ KROSS") 
       requests.post('https://hook.finandy.com/VMfD-y_3G5EgI5DUqFUK', json=FASTERLONG)

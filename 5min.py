4408075dd2ae49d2d245e1f0acc68e134cb5fa11
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-05-08 15:11:15 -0400

    Update 5min.py

--- 5min.py
+++ 5min.py
@@ -21,7 +21,7 @@ symbols = [
 
 def indicator(symbol):
   
-  kline = client.futures_historical_klines(symbol, "5m", "24 hours ago UTC+1",limit=1000)
+  kline = client.futures_historical_klines(symbol, "5m", "24 hours ago UTC+1",limit=500)
   df = pd.DataFrame(kline)
   
   if not df.empty:

3df67f3fa83c5598cb1c26c49fd3a614de7010cd
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-05-09 16:33:44 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -80,7 +80,7 @@ def run_strategy():
             
             if (df.iloc[-2]['Close'] > df.iloc[-2]['upperband']) and (df.iloc[-2]['diff'] >= 1) and (imbalance < -0.35):
                 
-              Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(df['diff'][-3],2)} \n💵 Precio: {df['Close'][-2]}\n📍 Picker: {round(imbalance,6)}")
+              Tb.telegram_canal_3por(f"⚡️ {symbol}\n🔴 SHORT\n⏳ 3 min \n🔝 Cambio: % {round(df.iloc[-2]['diff'],2)} \n💵 Precio: {df['Close'][-2]}\n📍 Picker: {round(imbalance,6)}")
              
               PORSHORT = {
                 "name": "CORTO 3POR",
@@ -95,7 +95,7 @@ def run_strategy():
               requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT)    
             elif (df.iloc[-2]['Close'] < df.iloc[-2]['lowerband']) and (df.iloc[-2]['diff'] >= 1) and (imbalance > 0.35):
                 
-              Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(df['diff'][-3],2)} \n💵 Precio: {df['Close'][-2]}\n📍 Picker: {round(imbalance,6)}") 
+              Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(df.iloc[-2]['diff'],2)} \n💵 Precio: {df['Close'][-2]}\n📍 Picker: {round(imbalance,6)}") 
               
               PORLONG = {
                 "name": "LARGO 3POR",

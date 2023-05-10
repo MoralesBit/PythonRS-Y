f8a07e1eda9b85b0ebce84cddd661a36931b71c2
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-05-09 22:52:34 -0400

    Actualizar 3min.py

--- 3min.py
+++ 3min.py
@@ -93,7 +93,7 @@ def run_strategy():
                 }
    
               requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT)    
-            elif (df.iloc[-2]['Close'] < df.iloc[-2]['lowerband']) and (df.iloc[-2]['diff'] >= 1) and (imbalance > 0.35):
+            if (df.iloc[-2]['Close'] < df.iloc[-2]['lowerband']) and (df.iloc[-2]['diff'] >= 1) and (imbalance > 0.35):
                 
               Tb.telegram_canal_3por(f"⚡️ {symbol}\n🟢 LONG\n⏳ 3 min \n🔝 Cambio: % {round(df.iloc[-2]['diff'],2)} \n💵 Precio: {df['Close'][-2]}\n📍 Picker: {round(imbalance,6)}") 
               

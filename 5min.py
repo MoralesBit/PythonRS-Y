8b711cfe807665b6c9ca80756fc8f8b9a0f719d6
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-05-11 09:52:15 -0400

    Update 5min.py

--- 5min.py
+++ 5min.py
@@ -39,6 +39,8 @@ def calculate_indicators(symbol):
     slowk, slowd = ta.STOCH(df['High'], df['Low'], df['Close'], fastk_period=5, slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
     df['slowk'] = slowk
     df['slowd'] = slowd
+    adx= ta.ADX(df['High'], df['Low'], df['Close'], timeperiod=14)
+    df['adx'] = adx
        
     return df[-3:]
   
@@ -55,48 +57,49 @@ def run_strategy():
                 continue
             # CONTRATENDENCIAs:
                        
-            if (float(df['slowk'][-3]) > float(df['slowd'][-3])) and (float(df['slowk'][-2]) < float(df['slowd'][-2])):           
+            #if (float(df['slowk'][-3]) > float(df['slowd'][-3])) and (float(df['slowk'][-2]) < float(df['slowd'][-2])):           
           
-             if (float(df['Close'][-3]) > float(df['upperband'][-3])) and (float(df['Close'][-2]) < float(df['upperband'][-2])) and (float(df['diff'][-3]) >= 1) and (df['rsi'][-2] > 70): 
+            # if (float(df['Close'][-3]) > float(df['upperband'][-3])) and (float(df['Close'][-2]) < float(df['upperband'][-2])) and (float(df['diff'][-3]) >= 1) and (df['rsi'][-2] > 70): 
                   
-              Tb.telegram_canal_3por(f"🔴 {symbol}\n🔝 Cambio: % {round(df['diff'][-3],2)} \n💵 Precio: {df['Close'][-2]}\n📍 Picker ⏳ 5 min")
+            #  Tb.telegram_canal_3por(f"🔴 {symbol}\n🔝 Cambio: % {round(df['diff'][-3],2)} \n💵 Precio: {df['Close'][-2]}\n📍 Picker ▫️ 5 min")
             
-              PICKERSHORT= {
-                "name": "PICKER SHORT",
-                "secret": "hgw3399vhh",
-                "side": "sell",
-                "symbol": symbol,
-                "open": {
-                "price": float(df['Close'][-1])
-                }
-                }
+            #  PICKERSHORT= {
+            #    "name": "PICKER SHORT",
+            #    "secret": "hgw3399vhh",
+            #    "side": "sell",
+            #    "symbol": symbol,
+            #    "open": {
+            #    "price": float(df['Close'][-1])
+            #    }
+            #    }
    
-              requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=PICKERSHORT)    
+            #  requests.post('https://hook.finandy.com/30oL3Xd_SYGJzzdoqFUK', json=PICKERSHORT)    
          
-            if (float(df['slowk'][-3]) < float(df['slowd'][-3])) and (float(df['slowk'][-2]) > float(df['slowd'][-2])):    
+            #if (float(df['slowk'][-3]) < float(df['slowd'][-3])) and (float(df['slowk'][-2]) > float(df['slowd'][-2])):    
                 
-             if (float(df['Close'][-3]) < float(df['lowerband'][-3])) and (float(df['Close'][-2]) > float(df['lowerband'][-2])) and (float(df['diff'][-3]) >= 1) and (df['rsi'][-2] < 30): 
+            # if (float(df['Close'][-3]) < float(df['lowerband'][-3])) and (float(df['Close'][-2]) > float(df['lowerband'][-2])) and (float(df['diff'][-3]) >= 1) and (df['rsi'][-2] < 30): 
                  
-              Tb.telegram_canal_3por(f"🟢 {symbol}\n🔝 Cambio: % {round(df['diff'][-3],2)} \n💵 Precio: {df['Close'][-2]}\n📍 Picker ⏳ 5 min") 
+            #  Tb.telegram_canal_3por(f"🟢 {symbol}\n🔝 Cambio: % {round(df['diff'][-3],2)} \n💵 Precio: {df['Close'][-2]}\n📍 Picker ▫️ 5 min") 
             
-              PICKERLONG = {
-               "name": "PICKER LONG",
-               "secret": "xjth0i3qgb",
-               "side": "buy",
-               "symbol": symbol,
-               "open": {
-               "price": float(df['Close'][-1]) 
-              }
-              }
-              requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=PICKERLONG) 
+            #  PICKERLONG = {
+            #   "name": "PICKER LONG",
+            #   "secret": "xjth0i3qgb",
+            #   "side": "buy",
+            #   "symbol": symbol,
+            #   "open": {
+            #   "price": float(df['Close'][-1]) 
+            #  }
+            #  }
+            #  requests.post('https://hook.finandy.com/lIpZBtogs11vC6p5qFUK', json=PICKERLONG) 
             
             #FISHING PISHA:
             
-            if (float(df['slowk'][-3]) < float(df['slowd'][-3])) and (float(df['slowk'][-2]) < float(df['slowd'][-2])):    
-                
-             if (float(df['Close'][-3]) < float(df['lowerband'][-3])) and (float(df['Close'][-2]) < float(df['lowerband'][-2])) and (float(df['diff'][-3]) >= 1): 
+            #if (float(df['slowk'][-3]) < float(df['slowd'][-3])) and (float(df['slowk'][-2]) < float(df['slowd'][-2])):    
+            if (40 < df['rsi'][-2] < 20) and (df['rsi'][-3] > df['rsi'][-2]):   
+                  
+             if (float(df['Close'][-3]) < float(df['lowerband'][-3])) and (float(df['diff'][-3]) >= 1) and (df['adx'][-3] < df['adx'][-2]) and (20 < df['adx'][-2] < 40): 
                  
-              Tb.telegram_canal_prueba(f"🔴 {symbol}\n💵 Precio: {df['Close'][-2]}\n🎣 Fishing Pisha ⏳ 5 min") 
+              Tb.telegram_canal_prueba(f"🔴 {symbol}\n💵 Precio: {df['Close'][-2]}\n🎣 Fishing Pisha ▫️ 5 min") 
             
               FISHINGSHORT = {
                 "name": "FISHING SHORT",
@@ -110,11 +113,12 @@ def run_strategy():
               requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=FISHINGSHORT) 
             
               
-            if (float(df['slowk'][-3]) > float(df['slowd'][-3])) and (float(df['slowk'][-2]) > float(df['slowd'][-2])):           
-          
-             if (float(df['Close'][-3]) > float(df['upperband'][-3])) and (float(df['Close'][-2]) > float(df['upperband'][-2])) and (float(df['diff'][-3]) >= 1): 
+            #if (float(df['slowk'][-3]) > float(df['slowd'][-3])) and (float(df['slowk'][-2]) > float(df['slowd'][-2])):           
+            if (60 < df['rsi'][-2] < 80) and (df['rsi'][-3] < df['rsi'][-2]): 
+                
+             if (float(df['Close'][-3]) > float(df['upperband'][-3])) and (float(df['diff'][-3]) >= 1) and (df['adx'][-3] < df['adx'][-2]) and (20 < df['adx'][-2] < 40): 
                   
-              Tb.telegram_canal_prueba(f"🟢 {symbol}\n💵 Precio: {df['Close'][-2]}\n🎣 Fishing Pisha ⏳ 5 min")            
+              Tb.telegram_canal_prueba(f"🟢 {symbol}\n💵 Precio: {df['Close'][-2]}\n🎣 Fishing Pisha ▫️ 5 min")            
               
               FISHINGLONG = {
                 "name": "FISHING LONG",

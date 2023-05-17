52bd0e5c94b8d871effe6548e32144644dba2a8c
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-05-17 07:27:19 -0400

    Actualizar 5min.py

--- 5min.py
+++ 5min.py
@@ -185,7 +185,7 @@ def run_strategy():
               
             
             if (float(df['Close'][-2]) >= df['ema_300'][-2]):
-                if (0.4) < market_sentiment_3 < market_sentiment_2:         
+                if (0.4) < market_sentiment_2:         
                    
                         Tb.telegram_send_message(f"🟢 {symbol} ▫️ {round(df['market_sentiment'][-2],2)}\n💵 Precio: {df['Close'][-2]}\n🎣 Fishing Pisha ▫️ 5 min ▫️ {round(df['weighted_bid_ratio'][-2],4)}")            
               

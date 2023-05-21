8601d5d9d83b3ab147b51de6a8c7cb3e0fc17d30
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-05-21 14:52:19 -0400

    Update 3min.py

--- 3min.py
+++ 3min.py
@@ -113,7 +113,7 @@ def run_strategy():
             #
                 
             if (df['rsi'][-2] > 70) and (upperband[-2] <= float(df['Close'][-2])):  
-                if 0 > market_sentiment_2:
+               
                     Tb.telegram_send_message(f"🟢 {symbol} ▫️ {round(df['market_sentiment'][-2],2)}\n💵 Precio: {df['Close'][-2]}\n📍 Picker ▫️ 3 min")
                     
                     PORSHORT = {
@@ -132,7 +132,7 @@ def run_strategy():
             #if df['market_sentiment'][-2] >= (var):
                 
             if (df['rsi'][-2] < 30) and (lowerband[-2] >= float(df['Close'][-2])):    
-                  if 0 < market_sentiment_2:
+                 
                     Tb.telegram_send_message(f"🔴 {symbol} ▫️ {round(df['market_sentiment'][-2],2)}\n💵 Precio: {df['Close'][-2]}\n📍 Picker ▫️ 3 min")
                                 
                     PORLONG = {

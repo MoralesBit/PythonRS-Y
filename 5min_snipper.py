42c2e468e83a172c0283369c0975bdcab1683f65
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-08-04 13:07:05 -0400

    Update 5min_snipper.py

--- 5min_snipper.py
+++ 5min_snipper.py
@@ -1,60 +1,45 @@
-import time
-import requests
+from binance.client import Client
 import pandas as pd
 import talib as ta
-from binance.client import Client
 import Telegram_bot as Tb
- 
+import  schedule as schedule
+import time as ti
+import requests
+import numpy as np
 
 Pkey = ''
 Skey = ''
+
 client = Client(api_key=Pkey, api_secret=Skey)
 
-def get_trading_symbols():
-    """Obtiene la lista de símbolos de futuros de Binance que están disponibles para trading"""
-    futures_info = client.futures_exchange_info()
-    symbols = [symbol['symbol'] for symbol in futures_info['symbols'] if symbol['status'] == "TRADING"]
-    #symbols = ["BTCUSDT", "XRPUSDT", "BNBUSDT", "ADAUSDT","DOGEUSDT", "SOLUSDT", "TRXUSDT", "LTCUSDT" , "MATICUSDT", "BCHUSDT", "AVAXUSDT","1000SHIBUSDT","LINKUSDT","XLMUSDT","UNIUSDT","ATOMUSDT","XMRUSDT","FILUSDT"]  
-    return symbols
+futures_info = client.futures_exchange_info()
 
-def calculate_indicators(symbol):
-        
-    klines = client.futures_klines(symbol=symbol, interval=Client.KLINE_INTERVAL_5MINUTE, limit=1000)
-    df = pd.DataFrame(klines)
-    if df.empty:
-        return None
-    df.columns = ['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume',
-                  'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore']
-    df['Open time'] = pd.to_datetime(df['Open time'], unit='ms')
-    
-    df = df.set_index('Open time')
-    
-    df[['Open', 'High', 'Low', 'Close']] = df[['Open', 'High', 'Low', 'Close']].astype(float)   
+symbols = [
+    symbol['symbol'] for symbol in futures_info['symbols']
+    if symbol['status'] == "TRADING"
+  ]
+#symbols = ["BTCUSDT", "XRPUSDT", "BNBUSDT", "ADAUSDT","DOGEUSDT", "SOLUSDT", "TRXUSDT", "LTCUSDT" , "MATICUSDT", "BCHUSDT", "AVAXUSDT","1000SHIBUSDT","LINKUSDT","XLMUSDT","UNIUSDT","ATOMUSDT","XMRUSDT","FILUSDT"]  
+
+def indicator(symbol):
+  
+  kline = client.futures_historical_klines(symbol, "5m", "24 hours ago UTC+1",limit=1000)
+  df = pd.DataFrame(kline)
+  
+  if not df.empty:
+    df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close',
+      'Quote_Volume', 'Trades_Count', 'BUY_VOL', 'BUY_VOL_VAL', 'x']
+    df['Date'] = pd.to_datetime(df['Date'], unit='ms')
+    df = df.set_index('Date')
+    df[['Open', 'High', 'Low', 'Close']] = df[['Open', 'High', 'Low', 'Close']].astype(float)
     df['diff'] = abs((df['High'] / df['Low'] -1) * 100)    
     upperband, middleband, lowerband = ta.BBANDS(df['Close'], timeperiod=20, nbdevup=2.5, nbdevdn=2.5, matype=0)
     df['upperband'] = upperband
     df['middleband'] = middleband
     df['lowerband'] = lowerband
-   
-    return df[-3:]
-        
-def run_strategy():
-    """Ejecuta la estrategia de trading para cada símbolo en la lista de trading"""
-    symbols = get_trading_symbols()
-       
-    for symbol in symbols:
-        print(symbol)
 
-        try:
-            df = calculate_indicators(symbol)
-                                                
-            if df is None:
-                continue
-            
-            if (df['Close'][-2]) >= (df['upperband'][-2]):
-          
-                        Tb.telegram_canal_prueba(f"🔴 {symbol} \n💵 Precio: {round(df['Close'][-1],4)}\n📍 Picker ▫️ 5 min")
-                        PICKERSHORT = {
+    if df['Close'][-2] >= df['upperband'][-2]:
+                    Tb.telegram_canal_prueba(f"🔴 {symbol} \n💵 Precio: {round(df['Close'][-1],4)}\n📍 Picker ▫️ 5 min")
+                    PICKERSHORT = {
                     "name": "PICKER SHORT",
                     "secret": "ao2cgree8fp",
                     "side": "sell",
@@ -63,12 +48,11 @@ def run_strategy():
                     "price": float(df['Close'][-1]) 
                     }
                     }
-                        requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PICKERSHORT) 
+                    requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PICKERSHORT) 
                     
-            elif (df['Close'][-2]) <= (df['lowerband'][-2]):
-
-                        Tb.telegram_canal_prueba(f"🟢 {symbol} \n💵 Precio: {round(df['Close'][-1],4)}\n📍 Picker  ▫️ 5 min")
-                        PICKERLONG = {
+    elif df['Close'][-2] <= df['lowerband'][-2]:
+                    Tb.telegram_canal_prueba(f"🟢 {symbol} \n💵 Precio: {round(df['Close'][-1],4)}\n📍 Picker  ▫️ 5 min")
+                    PICKERLONG = {
                     "name": "PICKER LONG",
                     "secret": "nwh2tbpay1r",
                     "side": "buy",
@@ -77,17 +61,17 @@ def run_strategy():
                     "price": float(df['Close'][-1])
                     }
                     }
-                        requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PICKERLONG)  
+                    requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PICKERLONG)  
                                                                  
-            else:
-                print("NO")                
+    else:
+                print("NO")
 
-        except Exception as e:
-          
-            print(f"Error en el símbolo {symbol}: {e}")
-
+               
 while True:
-    current_time = time.time()
-    seconds_to_wait = 300 - current_time % 300
-    time.sleep(seconds_to_wait)    
-    run_strategy()
+  current_time = ti.time()
+  seconds_to_wait = 300 - current_time % 300
+  ti.sleep(seconds_to_wait)   
+  
+  for symbol in symbols:
+      indicator(symbol)
+      print(symbol)

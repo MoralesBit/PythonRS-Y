9df1c89425d56e70420428d06659aebf314ca5b7
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-08-05 14:04:08 -0400

    Update 5min_snipper.py

--- 5min_snipper.py
+++ 5min_snipper.py
@@ -22,18 +22,20 @@ symbols = [
 
 def indicator(symbol):
   
-  kline = client.futures_historical_klines(symbol, "5m", "24 hours ago UTC+1",limit=1000)
-  df = pd.DataFrame(kline)
-  
+  url = f'https://fapi.binance.com/fapi/v1/klines?symbol={symbol}&interval=5m&startTime=2 days ago UTC+1&limit=500'
+  response = requests.get(url).json()
+  df = pd.DataFrame(response, columns=['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume', 'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore'])
+  df['Open time'] = pd.to_datetime(df['Open time'], unit='ms')
+  df = df.set_index('Open time')
   if not df.empty:
-    df.columns = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close',
-      'Quote_Volume', 'Trades_Count', 'BUY_VOL', 'BUY_VOL_VAL', 'x']
-    df['Date'] = pd.to_datetime(df['Date'], unit='ms')
-    df = df.set_index('Date')
       
     df[['Open', 'High', 'Low', 'Close']] = df[['Open', 'High', 'Low', 'Close']].astype(float)
-    df['diff'] = abs((df['High'] / df['Low'] -1) * 100)
-    
+    df['diff'] = ((df['High'] / df['Low'] -1) * 100)
+    df['upperband'], df['middleband'], df['lowerband'] = ta.BBANDS(df['Close'],
+                                               timeperiod=20,
+                                               nbdevup=2,
+                                               nbdevdn=2,
+                                               matype=0)
     #Imbalance
     depth = 3
 
@@ -55,7 +57,7 @@ def indicator(symbol):
     
     if imbalance < -0.8: 
       if df['diff'][-2] >= 1:
-                    Tb.telegram_canal_3por(f"🔴 {symbol} \n💵 Precio: {round(df['Close'][-1],4)}\n📍 Picker ▫️ 5 min")
+                    Tb.telegram_canal_prueba(f"🔴 {symbol} \n💵 Precio: {round(df['Close'][-1],4)}\n📍 Picker ▫️ 5 min")
                     PICKERSHORT = {
                     "name": "PICKER SHORT",
                     "secret": "ao2cgree8fp",
@@ -67,8 +69,8 @@ def indicator(symbol):
                     }
                     requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PICKERSHORT) 
     elif imbalance > 0.8:                 
-      if df['diff'][-2] >= 1:
-                    Tb.telegram_canal_3por(f"🟢 {symbol} \n💵 Precio: {round(df['Close'][-1],4)}\n📍 Picker  ▫️ 5 min")
+      if df['diff'][-2] <= -1:
+                    Tb.telegram_canal_prueba(f"🟢 {symbol} \n💵 Precio: {round(df['Close'][-1],4)}\n📍 Picker  ▫️ 5 min")
                     PICKERLONG = {
                     "name": "PICKER LONG",
                     "secret": "nwh2tbpay1r",

cc152ad29d00211855eda2899a0c146b9b62c935
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-08-29 15:29:07 -0400

    Update 5min_snipper.py

--- 5min_snipper.py
+++ 5min_snipper.py
@@ -75,7 +75,7 @@ def run_strategy():
             if df_btc['p_btc_short'][-2] == 1:       
                 if df['p_short'][-2] == 1 and df['ema_short'][-2] == 1 :
                     #if df['roc_signal'][-1] == 1 : 
-                        Tb.telegram_send_messagete(f"🔴 {symbol} \n💵 Precio: {df['Close'][-2]}\n📍 Fishing Pisha ▫️ 5 min")
+                        Tb.telegram_send_message(f"🔴 {symbol} \n💵 Precio: {df['Close'][-2]}\n📍 Fishing Pisha ▫️ 5 min")
                         FISHINGSHORT = {
                         "name": "FISHING SHORT",
                         "secret": "azsdb9x719",
@@ -85,7 +85,6 @@ def run_strategy():
                         "price": float(df['Close'][-2])
                         }
                         }
-   
                         requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=FISHINGSHORT) 
               
             if df_btc['p_btc_long'][-2] == 1: 
@@ -101,40 +100,38 @@ def run_strategy():
                         "price": float(df['Close'][-2])
                         }
                         }
-                        
                         requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=FISHINGLONG) 
             
             if df_btc['p_btc_short'][-2] == 1: 
                 if df['p_long'][-2] == 1 and df['ema_long'][-2] == 1 :
                     #if df['roc_signal'][-1] == 1 :    
-                
-                            Tb.telegram_canal_prueba(f"🔴 {symbol} \n💵 Precio Entrada: {df['Close'][-2]}\n📍 Picker")
-                            PORSHORT = {
-                            "name": "CORTO 3POR",
-                            "secret": "ao2cgree8fp",
-                            "side": "sell",
-                            "symbol": symbol,
-                            "open": {
-                            "price": float(df['Close'][-2]) 
-                            }
-                            }
-                            requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT)
+                        Tb.telegram_canal_prueba(f"🔴 {symbol} \n💵 Precio Entrada: {df['Close'][-2]}\n📍 Picker")
+                        PORSHORT = {
+                        "name": "CORTO 3POR",
+                        "secret": "ao2cgree8fp",
+                        "side": "sell",
+                        "symbol": symbol,
+                        "open": {
+                        "price": float(df['Close'][-2]) 
+                        }
+                        }
+                        requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT)
                                                 
             if df_btc['p_btc_long'][-2] == 1:       
                 if df['p_short'][-2] == 1 and df['ema_short'][-2] == 1 :
                     #if df['roc_signal'][-1] == 1 :        
                 
-                            Tb.telegram_canal_prueba(f"🟢 {symbol} \n💵 Precio Entrada: {df['Close'][-2]}\n📍 Picker")
-                            PORLONG = {
-                            "name": "LARGO 3POR",
-                            "secret": "nwh2tbpay1r",
-                            "side": "buy",
-                            "symbol": symbol,
-                            "open": {
-                            "price": float(df['Close'][-2])
-                            }
-                            }
-                            requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)              
+                        Tb.telegram_canal_prueba(f"🟢 {symbol} \n💵 Precio Entrada: {df['Close'][-2]}\n📍 Picker")
+                        PORLONG = {
+                        "name": "LARGO 3POR",
+                        "secret": "nwh2tbpay1r",
+                        "side": "buy",
+                        "symbol": symbol,
+                        "open": {
+                        "price": float(df['Close'][-2])
+                        }
+                        }
+                        requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)              
             
             #time.sleep(0.5)                 
                            

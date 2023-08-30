098b7f2ffbdc8bd71080b1239ba48fa228f187d9
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-08-30 09:22:23 -0400

    Update 5min_snipper.py

--- 5min_snipper.py
+++ 5min_snipper.py
@@ -81,7 +81,7 @@ def run_strategy():
             
             if df_btc['cci_signal'][-2] == 0:       
                 if df['p_short'][-2] == 1 and df['ema_short'][-2] == 1 and df['a_short'][-2] == 1:
-                    if df['roc_signal'][-1] == 1 : 
+                    #if df['roc_signal'][-1] == 1 : 
                         Tb.telegram_send_message(f"🔴 {symbol} \n💵 Precio: {df['Close'][-2]}\n📍 Fishing Pisha ▫️ 5 min")
                         FISHINGSHORT = {
                         "name": "FISHING SHORT",
@@ -96,7 +96,7 @@ def run_strategy():
               
             if df_btc['cci_signal'][-2] == 1:   
                 if df['p_long'][-2] == 1 and df['ema_long'][-2] == 1 and df['a_long'][-2] == 1:
-                    if df['roc_signal'][-1] == 1 :                                               
+                    #if df['roc_signal'][-1] == 1 :                                               
                         Tb.telegram_send_message(f"🟢 {symbol} \n💵 Precio: {df['Close'][-2]}\n📍 Fishing Pisha ▫️ 5 min")
                         FISHINGLONG = {
                         "name": "FISHING LONG",

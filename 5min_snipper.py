a0120fe918e998c89b307849dbfa30ac939951a5
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-09-05 09:12:12 -0400

    Update 5min_snipper.py

--- 5min_snipper.py
+++ 5min_snipper.py
@@ -34,11 +34,25 @@ def calculate_indicators(symbol,interval):
     df[['Open', 'High', 'Low', 'Close','Volume']] = df[['Open', 'High', 'Low', 'Close','Volume']].astype(float) 
       
     df['diff'] = abs((df['High'] / df['Low'] -1) * 100)
+    df['diff_signal'] = np.where(df['diff'] > 3,1,0)
+    
+    upperband, middleband, lowerband = ta.BBANDS(df['Close'],
+                                                timeperiod=20,
+                                                nbdevup=3,
+                                                nbdevdn=3,
+                                                matype=0)
+    df['upperband'] = upperband
+    df['middleband'] = middleband
+    df['lowerband'] = lowerband
+    
+    df['BB_signal_short'] = np.where(df['upperband'] < df['Close'],1,0)
+    df['BB_signal_long'] = np.where(df['lowerband'] > df['Close'],1,0)
     
     df['rsi'] = ta.RSI(df['Close'], timeperiod=14)
-    df['rsi_signal_long'] = np.where(df['rsi'] < 60,1,0)
-    df['rsi_signal_short'] = np.where(df['rsi'] > 40,1,0)
-   
+    df['rsi_signal_short'] = np.where(df['rsi'] > 70,1,0)
+    df['rsi_signal_long'] = np.where(df['rsi'] < 30,1,0)
+    
+    df['ema20'] = df['Close'].ewm(span=20, adjust=False).mean()
     df['ema200'] = df['Close'].ewm(span=200, adjust=False).mean()
     
     acceleration=0.02 
@@ -51,6 +65,9 @@ def calculate_indicators(symbol,interval):
     
     df['ema_short'] = np.where( df['ema200'] > df['Close'],1,0)
     df['ema_long'] = np.where( df['ema200'] < df['Close'],1,0)
+    
+    df['ema_short_20'] = np.where( df['ema20'] < df['Close'],1,0)
+    df['ema_long_20'] = np.where( df['ema20'] > df['Close'],1,0)
     
     df['roc'] = ta.ROC(df['Close'], timeperiod=288)
     
@@ -81,7 +98,7 @@ def run_strategy():
             
             if dfbtc['ema_short'][-2] == 1:      
                 if df['p_long'][-2] == 1 and df['ema_short'][-2] == 1:
-                    if df['roc_short'][-2] == 1 and df['cci_signal'][-2] == 0:
+                    if df['roc_short'][-2] == 1 and df['cci_signal'][-2] == 0 and df['ema_short_20'][-2] == 1:
                         Tb.telegram_send_message(f"🔴 {symbol} \n💵 Precio: {df['Close'][-2]}\n📊 {round(df['roc'][-2],3)}% \n⏳ 5M")
                         FISHINGSHORT = {
                         "name": "FISHING SHORT",
@@ -97,7 +114,7 @@ def run_strategy():
                 
             else :
                 if df['p_short'][-2] == 1 and df['ema_long'][-2] == 1:
-                    if df['roc_long'][-2] == 1  and df['cci_signal'][-2] == 1:                                               
+                    if df['roc_long'][-2] == 1  and df['cci_signal'][-2] == 1 and df['ema_long_20'][-2] == 1:                                             
                         Tb.telegram_send_message(f"🟢 {symbol} \n💵 Precio: {df['Close'][-2]}\n📊 {round(df['roc'][-2],3)}% \n⏳ 5M")
                         FISHINGLONG = {
                         "name": "FISHING LONG",
@@ -110,7 +127,36 @@ def run_strategy():
                         }
                         requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=FISHINGLONG) 
             
-                   
+            if df['BB_signal_short'][-2] == 1 :
+                if df['diff_signal'][-2] == 1 and df['rsi_signal_short'][-2] == 1:
+                        Tb.telegram_canal_prueba(f"🔴 {symbol} \n💵 Precio: {df['Close'][-2]}\n📊 {round(df['roc'][-2],3)}% \n⏳ 5M")
+                        PORSHORT = {
+                            "name": "CORTO 3POR",
+                            "secret": "ao2cgree8fp",
+                            "side": "sell",
+                            "symbol": symbol,
+                            "open": {
+                            "price": float(df['Close'][-2]) 
+                            }
+                            }
+   
+                
+                        requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT)
+              
+                
+            if df['BB_signal_long'][-2] == 1 :
+                if df['diff_signal'][-2] == 1 and df['rsi_signal_long'][-2] == 1:                                            
+                        Tb.telegram_canal_prueba(f"🟢 {symbol} \n💵 Precio: {df['Close'][-2]}\n📊 {round(df['roc'][-2],3)}% \n⏳ 5M")
+                        PORLONG = {
+                            "name": "LARGO 3POR",
+                            "secret": "nwh2tbpay1r",
+                            "side": "buy",
+                            "symbol": symbol,
+                            "open": {
+                            "price": float(df['Close'][-2])
+                            }
+                            }
+                        requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG)         
             
             
                         

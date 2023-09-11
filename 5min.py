88931fbfbbfd27f160f6d9071c09ef983b3ea8dc
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-09-11 15:06:53 -0400

    Update 5min.py

--- 5min.py
+++ 5min.py
@@ -13,101 +13,174 @@ client = Client(api_key=Pkey, api_secret=Skey)
 def get_trading_symbols():
     """Obtiene la lista de símbolos de futuros de Binance que están disponibles para trading"""
     futures_info = client.futures_exchange_info()
-    #symbols = [symbol['symbol'] for symbol in futures_info['symbols'] if symbol['status'] == "TRADING"]
-    symbols = ["XVSUSDT"]
-    #symbols.remove("ETHBTC")  
+    symbols = [symbol['symbol'] for symbol in futures_info['symbols'] if symbol['status'] == "TRADING"]
+    coins_to_remove = ["ETHBTC", "USDCUSDT", "BNBBTC", "ETHUSDT", "BTCDOMUSDT", "BTCUSDT_230929","XEMUSDT","BLUEBIRDUSDT","ETHUSDT_231229","DOGEUSDT","LITUSDT","ETHUSDT_230929","BTCUSDT_231229"]  # Lista de monedas a eliminar
+    for coin in coins_to_remove:
+        if coin in symbols:
+            symbols.remove(coin)
     return symbols
 
-   
-def calculate_indicators(symbol,interval):
-        
-    klines = client.futures_klines(symbol=symbol, interval=interval, limit=1000)
+def calculate_indicators(symbol, interval):
+    klines = client.futures_klines(symbol=symbol, interval=interval, limit=500)
     df = pd.DataFrame(klines)
+
     if df.empty:
         return None
+
     df.columns = ['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume',
                   'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore']
+
     df['Open time'] = pd.to_datetime(df['Open time'], unit='ms')
-    
     df = df.set_index('Open time')
-           
-    df[['Open', 'High', 'Low', 'Close','Volume']] = df[['Open', 'High', 'Low', 'Close','Volume']].astype(float) 
-      
-    df['retro'] = abs((df['Open'] / df['Close'] -1) * 100) 
-    df['retro_signal'] = np.where(df['retro'][-1] > 0.5,1,0)
+
+    df[['Open', 'High', 'Low', 'Close', 'Volume']] = df[['Open', 'High', 'Low', 'Close', 'Volume']].astype(float)
     
-    df['posicion_signal'] = np.where(df['Close'] < df['Open'],1,0)
+    df['rsi'] = ta.RSI(df['Close'], timeperiod=14)
+    df['sma'] = ta.SMA(df['rsi'], timeperiod=20)
+    df['sma_signal'] = np.where(df['sma'] < 50, 1, 0)
     
-    acceleration=0.02 
-    maximum=0.20
+    df['ema200'] = df['Close'].ewm(span=200, adjust=False).mean()
+    df['ema22'] = df['Close'].ewm(span=22, adjust=False).mean()
     
+    df['cross_down'] = np.where(df['Close'][-3] > df['ema22'][-3] and  df['Close'][-2] < df['ema22'][-2] and df['ema200'][-2] > df['ema22'][-2], 1, 0)
+    df['cross_up'] = np.where(df['Close'][-3] < df['ema22'][-3] and  df['Close'][-2] > df['ema22'][-2] and df['ema200'][-2] < df['ema22'][-2], 1, 0)
+    
+   
+    #df['ema_long'] = np.where(df['Close'] > df['ema22'] > df['ema200'] , 1, 0)
+    #df['ema_short'] = np.where(df['Close'] > df['ema22'] > df['ema200'] , 1, 0)
+
+    acceleration=0.02
+    maximum=1
+
     df['psar'] = ta.SAR(df['High'], df['Low'], acceleration, maximum)
-    df['psar_signal'] = np.where(df['psar'] > df['Close'], 1,0)
+
+    df['p_short'] = np.where(df['psar'] > df['Close'], 1, 0)
+    df['p_long'] = np.where(df['psar'] < df['Close'], 1, 0)
     
-    macd, signal, hist = ta.MACD(df['Close'], 
-                                    fastperiod=12, 
-                                    slowperiod=26, 
-                                    signalperiod=9)
-  
-    df['macd'] = macd
-    df['signal'] = signal
-    df['hist'] = hist
+    df['p_cross_short'] = np.where(df['psar'][-3] < df['Close'][-3] and df['psar'][-2] > df['Close'][-2], 1, 0)
+    df['p_cross_long'] = np.where(df['psar'][-3] > df['Close'][-3] and df['psar'][-2] < df['Close'][-2], 1, 0)
     
-    df['macd_long'] = np.where( df['hist'][-3] < df['hist'][-2],1,0)
-    df['macd_short'] = np.where( df['hist'][-3] > df['hist'][-2],1,0)  
+    df['cci'] = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=28)
+    df['cci_signal'] = np.where(df['cci'][-2] > 0,1,0)
 
     return df[-3:]
-        
+
+
 def run_strategy():
     """Ejecuta la estrategia de trading para cada símbolo en la lista de trading"""
     symbols = get_trading_symbols()
-       
-    for symbol in symbols:
-                           
-        print(symbol)
+    import time
+import numpy as np
+import requests
+import pandas as pd
+import talib as ta
+from binance.client import Client
+import Telegram_bot as Tb
+
+Pkey = ''
+Skey = ''
+client = Client(api_key=Pkey, api_secret=Skey)
+
+def get_trading_symbols():
+    """Obtiene la lista de símbolos de futuros de Binance que están disponibles para trading"""
+    futures_info = client.futures_exchange_info()
+    symbols = [symbol['symbol'] for symbol in futures_info['symbols'] if symbol['status'] == "TRADING"]
+    coins_to_remove = ["ETHBTC", "USDCUSDT", "BNBBTC", "ETHUSDT", "BTCDOMUSDT", "BTCUSDT_230929","XEMUSDT","BLUEBIRDUSDT","ETHUSDT_231229","DOGEUSDT","LITUSDT","ETHUSDT_230929","BTCUSDT_231229"]  # Lista de monedas a eliminar
+    for coin in coins_to_remove:
+        if coin in symbols:
+            symbols.remove(coin)
+    return symbols
+
+def calculate_indicators(symbol, interval):
+    klines = client.futures_klines(symbol=symbol, interval=interval, limit=500)
+    df = pd.DataFrame(klines)
+
+    if df.empty:
+        return None
+
+    df.columns = ['Open time', 'Open', 'High', 'Low', 'Close', 'Volume', 'Close time', 'Quote asset volume',
+                  'Number of trades', 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Ignore']
+
+    df['Open time'] = pd.to_datetime(df['Open time'], unit='ms')
+    df = df.set_index('Open time')
+
+    df[['Open', 'High', 'Low', 'Close', 'Volume']] = df[['Open', 'High', 'Low', 'Close', 'Volume']].astype(float)
+    
+    df['diff'] = abs((df['High'] / df['Low'] -1) * 100)
+      
+    precio_high = (max(df['Close']))
+    precio_low = (min(df['Close']))
+    diff_precio = precio_high - precio_low
+    
+    df['nivel_062'] = precio_high - (0.62)*(diff_precio)
+    df['nivel_236'] = precio_high - (0.236)*(diff_precio)
+    
+    df['short'] = np.where(df['nivel_062'][-3] < df['Close'][-3] and df['nivel_062'][-2] > df['Close'][-2],1,0)
+    df['long'] = np.where(df['nivel_236'][-3] > df['Close'][-3] and df['nivel_236'][-2] < df['Close'][-2],1,0)
         
+    return df[-3:]
+
+
+def run_strategy():
+    """Ejecuta la estrategia de trading para cada símbolo en la lista de trading"""
+    symbols = get_trading_symbols()
+   
+    
+    for symbol in symbols:
+        print(symbol)
+
         try:
-            df = calculate_indicators(symbol,interval=Client.KLINE_INTERVAL_5MINUTE)
-                                                            
+            df = calculate_indicators(symbol, interval=Client.KLINE_INTERVAL_5MINUTE)
+            print(df['fibo'][-2])
+
             if df is None:
                 continue
-           
-            if df['psar_signal'][-1] == 1 and df['retro_signal'][-1] == 1 and df['posicion_signal'][-1] == 0 and df['macd_short'][-2] == 1:
-                Tb.telegram_canal_prueba(f"🔴 {symbol} \n💵 Precio: {df['Close'][-2]}\n🚀 Fast and Fury")
-                PORSHORT = {
-                            "name": "PICKER SHORT",
-                            "secret": "ao2cgree8fp",
+   
+            if df['short'][-2] == 1:
+                        
+                            message = f"🔴 {symbol} \n💵 Precio: {df['Close'][-2]}"
+                            Tb.telegram_canal_3por(message)
+
+                            FISHINGSHORT = {
+                            "name": "FISHING SHORT",
+                            "secret": "azsdb9x719",
                             "side": "sell",
                             "symbol": symbol,
                             "open": {
-                            "price": float(df['Open'][-1]) 
+                                "price": float(df['Close'][-2])
                             }
                             }
-   
-                
-                requests.post('https://hook.finandy.com/a58wyR0gtrghSupHq1UK', json=PORSHORT)
-                        
-            if df['psar_signal'][-1] == 0 and df['retro_signal'][-1] == 1 and df['posicion_signal'][-1] ==1 and df['macd_long'][-2] == 1:
-                Tb.telegram_canal_prueba(f"🟢 {symbol} \n💵 Precio: {df['Close'][-2]}\n🚀 Fast and Fury")
-                PORLONG = {
-                            "name": "PICKER LONG",
-                            "secret": "nwh2tbpay1r",
+
+                            requests.post('https://hook.finandy.com/q-1NIQZTgB4tzBvSqFUK', json=FISHINGSHORT)
+
+            if df['long'][-2] == 1:
+                                               
+                            message = f"🟢 {symbol} \n💵 Precio: {df['Close'][-2]}"
+                            Tb.telegram_canal_3por(message)
+
+                            FISHINGLONG = {
+                            "name": "FISHING LONG",
+                            "secret": "0kivpja7tz89",
                             "side": "buy",
                             "symbol": symbol,
                             "open": {
-                            "price": float(df['Open'][-1])
+                                "price": float(df['Close'][-2])
                             }
                             }
-                requests.post('https://hook.finandy.com/o5nDpYb88zNOU5RHq1UK', json=PORLONG) 
+
+                            requests.post('https://hook.finandy.com/OVz7nTomirUoYCLeqFUK', json=FISHINGLONG)
+
+            else:
+                print("NEXT")
+                
             
-            time.sleep(0.25)            
+            
         except Exception as e:
-          
             print(f"Error en el símbolo {symbol}: {e}")
+
 
 while True:
     current_time = time.time()
-    seconds_to_wait = 0.25 - current_time % 0.25
-    time.sleep(seconds_to_wait)    
+    seconds_to_wait = 300 - current_time % 300
+    #time.sleep(seconds_to_wait)
     run_strategy()
-    #VERSION ESTABLE

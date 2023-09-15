6bc92f0ca3f7985614bac8a1e7073c941d8a14e9
MoralesBit <105336888+MoralesBit@users.noreply.github.com>
2023-09-15 13:55:40 -0400

    Update 5min_snipper.py

--- 5min_snipper.py
+++ 5min_snipper.py
@@ -50,8 +50,8 @@ def calculate_indicators(symbol,interval):
     
     df['roc'] = ta.ROC(df['Close'], timeperiod=288)
     
-    df['roc_long'] = np.where(df['roc'][-2] > 7,1,0)
-    df['roc_short'] = np.where(df['roc'][-2] < -7,1,0)
+    df['roc_long'] = np.where(df['roc'][-2] > 8,1,0)
+    df['roc_short'] = np.where(df['roc'][-2] < -8,1,0)
     
     df['cci'] = ta.CCI(df['High'], df['Low'], df['Close'], timeperiod=28)
     df['cci_signal'] = np.where(df['cci'][-2] > 0,1,0)
